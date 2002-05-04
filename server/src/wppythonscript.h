66cb2e46380ff11e32b1e56bce34dbf4b48da10a
thiagocorrea <thiagocorrea@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2002-05-04 00:57:54 +0000

    no message

--- server/src/wppythonscript.h
+++ server/src/wppythonscript.h
@@ -1,13 +1,36 @@
-// WPPythonScript.h: Schnittstelle für die Klasse WPPythonScript.
+//==================================================================================
 //
-//////////////////////////////////////////////////////////////////////
+//      Wolfpack Emu (WP)
+//	UO Server Emulation Program
+//
+//	Copyright 1997, 98 by Marcus Rating (Cironian)
+//  Copyright 2001 by holders identified in authors.txt
+//	This program is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation; either version 2 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program; if not, write to the Free Software
+//	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//	* In addition to that license, if you are running this program or modified
+//	* versions of it on a public system you HAVE TO make the complete source of
+//	* the version used by you available or provide people with a location to
+//	* download it.
+//
+//
+//
+//	Wolfpack Homepage: http://wpdev.sf.net/
+//========================================================================================
 
 #if !defined(AFX_WPPYTHONSCRIPT_H__F98DC0D9_AE54_46A6_9557_729DBDD07C03__INCLUDED_)
 #define AFX_WPPYTHONSCRIPT_H__F98DC0D9_AE54_46A6_9557_729DBDD07C03__INCLUDED_
-
-#if _MSC_VER > 1000
-#pragma once
-#endif // _MSC_VER > 1000
 
 #include "wpdefaultscript.h"
 #include "Python.h"
@@ -134,8 +157,8 @@ int Py_WPItemSetAttr( Py_WPItem *self, char *name, PyObject *value );
 PyObject* Py_WPItembySerial( PyObject* self, PyObject* args );
 
 // WPItem
-PyObject* Py_WPItem_update( PyObject* self, PyObject* args );
-PyObject* Py_WPItem_delete( PyObject* self, PyObject* args );
+PyObject* Py_WPItem_update( Py_WPItem* self, PyObject* args );
+PyObject* Py_WPItem_delete( Py_WPItem* self, PyObject* args );
 
 static PyTypeObject Py_WPItemType = {
     PyObject_HEAD_INIT(NULL)
