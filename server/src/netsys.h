bb0d5b7f5897cd5eafbbe87a5dd2cc4ceab7224e
thiagocorrea <thiagocorrea@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2002-06-26 20:23:04 +0000

    linux fix

--- server/src/netsys.h
+++ server/src/netsys.h
@@ -70,9 +70,9 @@ void signal_handler(int) ;
 // NETWORK defines
 #define SHUT_RDWR SD_BOTH
 
-#ifndef socklen_t
-#define socklen_t  int FAR 
-#endif
+//#ifndef socklen_t
+//#define socklen_t  int FAR 
+//#endif
 
 #define in_addr_t  UI32
 #define bzero(ptr,n)				memset(ptr,0,n)
