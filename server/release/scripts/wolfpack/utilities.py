d4b8ec2a7fefe0a77fc2f61980beaad394a8dc1a
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2002-12-10 22:50:52 +0000

    *** empty log message ***

---
+++ server/release/scripts/wolfpack/utilities.py
@@ -0,0 +1,46 @@
+
+# STAT MODIFIER (Bless, Curse, etc.)
+
+def statmodifier_dispel( char, args, source, dispelargs ):
+	# Normally reduce "if not 'silent' in dispelargs"
+	char.str -= args[0]
+	char.dex -= args[1]
+	char.int -= args[2]
+
+	if not "silent" in dispelargs:
+		# If Str changed, resend to others
+		if args[0] != 0:
+			char.updatehealth()
+		char.updatestats()
+
+	return
+
+def statmodifier_expire( char, args ):
+	# The stat modifier normally expired, so just reduce the stats and resend
+	char.str -= args[0]
+	char.dex -= args[1]
+	char.int -= args[2]
+
+	# If Str changed, resend to others
+	if args[0] != 0:
+		char.updatehealth()
+	char.updatestats()
+
+	return
+
+def statmodifier( char, time, mStr, mDex, mInt ):
+	# Dispel any old stat modifiers on this character
+	# And readd a new one (remove the old ones silently)
+	char.dispel( char, 0, "statmodifier", [ "silent" ] )
+
+	char.str += mStr
+	char.dex += mDex
+	char.int += mInt
+
+	if mStr != 0:
+		char.updatehealth()
+
+	char.updatestats()
+
+	# Save the values in a tempeffect to remove them later
+	char.addtimer( time, "wolfpack.utilities.statmodifier_expire", [ mStr, mDex, mInt ], 1, 1, "statmodifier", "wolfpack.utilities.statmodifier_dispel" )
