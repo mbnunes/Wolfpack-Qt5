d5bb037cf97a51ff12906864135ff6ca0fb744a1
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2002-12-10 21:27:05 +0000

    *** empty log message ***

--- server/release/scripts/wolfpack/__init__.py
+++ server/release/scripts/wolfpack/__init__.py
@@ -1,4 +1,18 @@
 
+import _wolfpack
+
+def finditem( serial ):
+	if serial <= 0x40000000:
+		raise TypeError, "Invalid Serial <= 0x40000000"
+	else:
+		return _wolfpack.finditem( int( serial ) )
+
+def findchar( serial ):
+	if serial >= 0x40000000:
+		raise TypeError, "Invalid Serial >= 0x40000000"
+	else:
+		return _wolfpack.findchar( int( serial ) )
+
 # Skill Constants
 ALCHEMY = 0
 ANATOMY = 1
