e2f1a3e896c702c7972645462835c0b8966ecda2
codex_ <codex_@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2002-12-13 14:18:32 +0000

    Added 'info dispatch

--- server/release/scripts/daggermenu.py
+++ server/release/scripts/daggermenu.py
@@ -1,5 +1,9 @@
 import wolfpack
+from wolfpack.gumps import cGump
 
 def onContextEntry( char, item, tag  ):
-    char.message( str( tag )  )
+    if( tag == 4 ):
+	char.message( "'info " + str( item.serial )  )
+    if( tag == 3 ):
+        
     return 1
