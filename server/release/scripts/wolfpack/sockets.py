5ab283c0139c07c65ec21bf9b506c689c4acdfcd
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2003-01-07 22:09:28 +0000

    *** empty log message ***

---
+++ server/release/scripts/wolfpack/sockets.py
@@ -0,0 +1,18 @@
+#===============================================================#
+#   )      (\_     | WOLFPACK 13.0.0 Scripts                    #
+#  ((    _/{  "-;  | Created by: DarkStorm                      #
+#   )).-' {{ ;'`   | Revised by:                                #
+#  ( (  ;._ \\ ctr | Last Modification: Created                 #
+#===============================================================#
+
+import _wolfpack.sockets
+
+def first():
+	return _wolfpack.sockets.first()
+
+def next():
+	return _wolfpack.sockets.next()
+
+def count():
+	return _wolfpack.sockets.count()
+
