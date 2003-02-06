80448345098bdae6ea319b904f792bc1b17d7381
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2003-02-06 21:03:21 +0000

    *** empty log message ***

--- server/release/scripts/commands.py
+++ server/release/scripts/commands.py
@@ -3,8 +3,19 @@ import wolfpack
 
 def onLoad():
 	wolfpack.registercommand( "test", "commands" )
-
+	wolfpack.registercommand( "sendpacket", "commands" )
 
 def onCommand( socket, command, argstring ):
-	socket.sysmessage( "test" )
+	if command == "sendpacket":
+		sendpacket( socket, command, argstring )
 	
+def sendpacket( socket, command, argstring ):
+	# Build a list
+	packet = []
+
+	pieces = argstring.split( " " )
+	for piece in pieces:
+		# Treat as hexadecimal digit (FF)
+		packet.append( int( piece, 16 ) )
+
+	socket.sendpacket( packet )
