ef70421c99b8a961d005fac27c2f30e2533be7d0
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2003-02-06 18:10:59 +0000

    *** empty log message ***

---
+++ server/release/scripts/potions.py
@@ -0,0 +1,28 @@
+
+# POTIONS SCRIPT
+# This script in general has all functions for potions
+# more1 defines the type of the potion
+# more2 the strength
+
+potions = {
+	1: healPotion
+}
+
+def onUse( char, item ):
+	# Potions need to be on your body to use them.
+	if item.getoutmostchar() != char:
+		char.message( "This potion needs to be in your belongings to use it." )
+		return 1
+
+	pType = item.more1
+
+	if not potions.has_key( pType ):
+		char.socket.sysmessage( "This potion has an unknown type: %u" % pType )
+		return 1
+	
+	potions[ pType ]( char, item )
+	return 1
+
+# Heal potion
+def healPotion( char, potion ):
+	char.message( "This potion will heal you" )
