0db84973113f6b943f409d1c84375096fc968c1d
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2003-02-28 23:04:10 +0000

    *** empty log message ***

---
+++ server/release/scripts/skills/fishing.py
@@ -0,0 +1,43 @@
+#################################################################
+#   )      (\_     # WOLFPACK 13.0.0 Scripts                    #
+#  ((    _/{  "-;  # Created by: DarkStorm                      #
+#   )).-' {{ ;'`   # Revised by:                                #
+#  ( (  ;._ \\ ctr # Last Modification: Created                 #
+#################################################################
+
+from wolfpack.consts import *
+import wolfpack
+
+# This is the rather complex fishing script
+
+def onUse( char, item ):
+	# Check if he used a fishing pole
+	if item.id != 0xdbf and item.id != 0xdc0:
+		return 0
+
+	socket = char.socket
+
+	# Can't fish on horses
+	if char.itemonlayer( LAYER_MOUNT ):
+		socket.clilocmessage( 0x7A4EB, "", 0x3b2, 3 ) # You can't fish while riding!
+		return 1
+
+	# Are we already fishing?
+	if socket.hastag( 'is_fishing' ):
+		socket.clilocmessage( 0x7A4EC, "", 0x3b2, 3 ) # You are already fishing.
+		return 1
+
+	# Assign the target request
+	socket.clilocmessage( 0x7A4EE, "", 0x3b2, 3 ) # What water do you want to fish in?
+	socket.attachtarget( "skills.fishing.response" )
+
+	return 1
+
+def response( char, args, target ):
+	# SkillFail: 
+	# SkillSuccess:
+	# Too Far away to do that: ID: 0x7A4F0 (0) / You need to be closer to the water to fish!
+	# There are impassables with z >= water.z and z <= water.z + 13: 0x7A4F5 You can't reach the water there.
+	# No water there: 0x7A4F2 You need water to fish in!
+
+	pass
