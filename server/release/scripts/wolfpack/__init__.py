8d9e760b74e260bc11d84627b55c6e7ca8346c08
codex_ <codex_@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2003-04-06 14:45:29 +0000

    added wolfpack.chars() - returns all chars in region

--- server/release/scripts/wolfpack/__init__.py
+++ server/release/scripts/wolfpack/__init__.py
@@ -58,6 +58,12 @@ def items( x, y, map, exact=1 ):
 	else:
 		return _wolfpack.items( x, y, map, exact )
 
+def chars( x, y, map, exact=1 ):
+	if not type( x ) is IntType or not type( y ) is IntType or not type( map ) is IntType or not type( exact ) is IntType:
+		raise TypeError, "x, y and map need to be integer values"
+	else:
+		return _wolfpack.chars( x, y, map, exact )
+
 def allitemsserials():
 	return _wolfpack.allitemsserials()
 
