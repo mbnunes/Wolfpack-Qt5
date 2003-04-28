ab3435e1a7d3555025057c093a166c6da0249101
khpae <khpae@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2003-04-28 05:27:38 +0000

    *** empty log message ***

---
+++ server/release/scripts/skills/stealth.py
@@ -0,0 +1,60 @@
+#################################################################
+#   )      (\_     # WOLFPACK 13.0.0 Scripts                    #
+#  ((    _/{  "-;  # Created by: khpae                          #
+#   )).-' {{ ;'`   # Revised by:                                #
+#  ( (  ;._ \\ ctr # Last Modification: Created                 #
+#################################################################
+
+from wolfpack.consts import *
+from wolfpack.utilities import *
+from wolfpack.time import *
+import wolfpack
+
+STEALTH_DELAY = 5000
+# the hiding skill before you can use the stealth skill
+MIN_HIDING = 800
+
+def onLoad():
+	wolfpack.registerglobal( HOOK_CHAR, EVENT_SKILLUSE, "skills.stealth" )
+
+def onSkillUse( char, skill ):
+	if skill != STEALTH:
+		return 0
+
+	if char.hastag( 'skill_delay' ):
+		cur_time = servertime()
+		if cur_time < char.gettag( 'skill_delay' ):
+			char.socket.clilocmessage( 500118, "", 0x3b2, 3 )
+			return 1
+		else:
+			char.deltag( 'skill_delay' )
+
+	# use hiding first
+	if not char.hidden:
+		char.socket.clilocmessage( 502725, "", 0x3b2, 3 )
+		return 1
+
+	# you can use stealth only when your hiding skill is over 80.0
+	if char.skill[ HIDING ] < MIN_HIDING:
+		char.socket.clilocmessage( 502726, "", 0x3b2, 3 )
+		return 1
+
+	# TODO :
+	# too much armor
+	# too many chars around
+	# needed ? if riding an animal
+
+	success = char.checkskill( STEALTH, 0, 1000 )
+
+	if success:
+		char.socket.clilocmessage( 502730, "", 0x3b2, 3 )
+		char.stealth = 0
+	else:
+		char.socket.clilocmessage( 502731, "", 0x3b2, 3 )
+		char.hidden = 0
+	
+	cur_time = servertime()
+	char.settag( 'skill_delay', cur_time + STEALTH_DELAY )
+
+	return 1
+
