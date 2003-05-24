6a4478466256b97d5bbc550384b9a08c9ed631d8
thiagocorrea <thiagocorrea@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2003-05-24 15:51:10 +0000

    Preferences class exported to python.

---
+++ server/release/scripts/wolfpack/settings.py
@@ -0,0 +1,35 @@
+#===============================================================#
+#   )      (\_     | WOLFPACK 13.0.0 Scripts                    #
+#  ((    _/{  "-;  | Created by: Correa                         #
+#   )).-' {{ ;'`   | Revised by:                                #
+#  ( (  ;._ \\ ctr | Last Modification: Created                 #
+#===============================================================#
+# Module with timing functions									#
+#===============================================================#
+
+import _wolfpack.settings
+import _wolfpack
+
+def getBool( section, key, default ):
+	return _wolfpack.settings.getBool()
+
+def setBool( section, key, value ):
+	return _wolfpack.settings.setBool( section, key, value )
+
+def getNumber( section, key, default ):
+	return _wolfpack.settings.getNumber( section, key, default )
+
+def setNumber( section, key, value ):
+	return _wolfpack.settings.setNumber( section, key, value )
+
+def getString( section, key, default ):
+	return _wolfpack.settings.getString( section, key, default )
+
+def setString( section, key, value ):
+	return _wolfpack.settings.setString( section, key, value )
+
+def reload():
+	return _wolfpack.settings.reload()
+
+def save():
+	return _wolfpack.settings.save()
