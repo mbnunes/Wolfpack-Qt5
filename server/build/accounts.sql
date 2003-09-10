b905e700a0e4f518463ae3e57e8fefb207b60f38
thiagocorrea <thiagocorrea@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2003-09-10 21:09:20 +0000

    fix to work with sqlite

--- server/build/accounts.sql
+++ server/build/accounts.sql
@@ -1,10 +1,9 @@
-CREATE TABLE `accounts` (
-  `login` varchar(255) NOT NULL default '',
-  `PASSWORD` varchar(255) NOT NULL default '',
-  `flags` int(11) NOT NULL default '0',
-  `acl` varchar(255) NOT NULL default 'player',
-  `lastlogin` int(11) NOT NULL default '0',
-  `blockuntil` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`login`),
-  UNIQUE KEY `login` (`login`)
-) TYPE=MyISAM;
+CREATE TABLE accounts (
+  login varchar(255) NOT NULL default '',
+  password varchar(255) NOT NULL default '',
+  flags int NOT NULL default '0',
+  acl varchar(255) NOT NULL default 'player',
+  lastlogin int NOT NULL default '0',
+  blockuntil int NOT NULL default '0',
+  PRIMARY KEY  (login)
+);
