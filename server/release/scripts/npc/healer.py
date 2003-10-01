9cee8016dc7a2d863c2c730b727b934f4ceec446
codex_ <codex_@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2003-10-01 09:19:22 +0000

    npc healer ( resurrect context menu )

---
+++ server/release/scripts/npc/healer.py
@@ -0,0 +1,29 @@
+#################################################################
+#   )      (\_     # WOLFPACK 13.0.0 Scripts                    #
+#  ((    _/{  "-;  # Created by: codex                          #
+#   )).-' {{ ;'`   # Revised by:                                #
+#  ( (  ;._ \\ ctr # Last Modification: Created                 #
+#################################################################
+
+from wolfpack.consts import *
+import wolfpack
+
+def onContextEntry( char, healer, tag  ):
+    if( tag == 1 ):
+	if not char or not healer:
+		return OOPS
+
+	if healer.dead:
+		char.socket.clilocmessage( 501040, "", YELLOW, NORMAL ) # The resurrecter must be alive
+		return OOPS
+
+	if char.dead:
+		char.socket.clilocmessage( 3002069, "", YELLOW, NORMAL ) # Resurrection
+		char.resurrect()
+		return OK
+	else:
+		char.socket.clilocmessage( 1060197, "", YELLOW, NORMAL ) # You are not dead, and thus cannot be resurrected!
+		return OOPS
+		
+	return OK
+
