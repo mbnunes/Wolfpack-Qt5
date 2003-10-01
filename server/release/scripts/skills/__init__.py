f0f9e47591f20fe3764aa7fd55748060e3de33f0
codex_ <codex_@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2003-10-01 22:23:06 +0000

    mining

--- server/release/scripts/skills/__init__.py
+++ server/release/scripts/skills/__init__.py
@@ -1,4 +1,3 @@
-
 import wolfpack
 from wolfpack.consts import *
 
@@ -19,3 +18,22 @@ def onSkillUse( char, skill ):
 		return 1
 		
 	return 0
+
+# table structure - look mining.py
+def successharvest( char, gem, table, resname, amount ):
+	socket = char.socket
+	message = table[ resname ][ SUCCESSCLILOC ]
+	backpack = char.getbackpack()
+	if not backpack:
+		return OOPS
+	resourceitem = wolfpack.newitem( 1 ) # Get new serial
+	resourceitem.decay = 1
+	resourceitem.color = table[ resname ][ COLORID ]
+	resourceitem.id = table[ resname ][ RESOURCEID ]
+	resourceitem.amount = amount
+        gem.amount = gem.amount - amount
+
+	backpack.additem( resourceitem )
+
+	socket.clilocmessage( table[ resname ][ SUCCESSCLILOC ], "", YELLOW, NORMAL )
+	return OK
--- server/release/scripts/skills/mining.py
+++ server/release/scripts/skills/mining.py
@@ -14,29 +14,52 @@ from wolfpack.utilities import *
 
 #mining calling from pickaxe.py and shovel.py
 
-# Name, reqSkill, minSkill, maxSkill, successClilocId, mutateVeinChance%, chanceToFail%
+
+# Name, reqSkill, minSkill, maxSkill, successClilocId, itemId, color, mutateVeinChance%, VeinChanceToFallback%
+
 oretable = \
-[
-['iron',	0,	0,	100,	1007072,	49,6,	0],
-['dullcopper',	65,	25,	105,	1007073,	11,2,	50],
-['shadowiron',	70,	30,	110,	1007074,	9,8,	50], 
-['copper',	75,	35,	115,	1007075,	8,4,	50],
-['bronze',	80,	40,	120,	1007076,	7,	50],
-['gold',	85,	45,	125,	1007077,	5,6,	50],
-['agapite',	90,	50,	130,	1007078,	4,2,	50],
-['verite',	95,	55,	135,	1007079,	2,8,	50],        
-['valorite',	99,	59,	139,	1007080,	1,4,	50]
-]
+{
+'iron':		[ 0,   0, 100, 1007072, 0x19b9,     0, 49.6, 0  ],
+'dullcopper':	[ 65, 25, 105, 1007073, 0x19b9, 0x973, 11.2, 50 ],
+'shadowiron':	[ 70, 30, 110, 1007074, 0x19b9, 0x966,  9.8, 50 ], 
+'copper':	[ 75, 35, 115, 1007075, 0x19b9, 0x960,  8.4, 50 ],
+'bronze':	[ 80, 40, 120, 1007076, 0x19b9, 0x972,    7, 50 ],
+'gold':		[ 85, 45, 125, 1007077, 0x19b9, 0x8a5,  5.6, 50 ],
+'agapite':	[ 90, 50, 130, 1007078, 0x19b9, 0x979,  4.2, 50 ],
+'verite':	[ 95, 55, 135, 1007079, 0x19b9, 0x89f,  2.8, 50 ],
+'valorite':	[ 99, 59, 139, 1007080, 0x19b9, 0x8ab,  1.4, 50 ]
+}
 
 def mining( char, pos, tool ):
    socket = char.socket
 
-   if char.hastag('mining_gem'):
+   if not char.hastag('mining_gem'):
+      veingem = getvein( socket, pos )
+      char.settag('mining_gem', veingem.serial )
+   else:
       gemserial = char.gettag('mining_gem')
+      veingem = wolfpack.finditem( gemserial )
+      
+   if veingem and char.distanceto( veingem ) > MINING_MAX_DISTANCE:
+      veingem = getvein( socket, pos )
 
-   veingem = getvein( socket, pos )
+   resname = veingem.gettag( 'resname' ) # Sometimes mutated in colored ore and back
    resourcecount = veingem.gettag( 'resourcecount' )
-   socket.sysmessage( 'Mining in progress... Total resourcecount here:' + str( resourcecount )  )
+   reqskill = oretable[ resname ][ REQSKILL ]
+
+   # Are you skilled enough ? And here is ore ?
+   if resourcecount > 2 and char.skill[ MINING ] > reqskill:
+      # Anyway you haven't 100% chance to get something :)
+      if char.skill[ MINING ] > reqskill:
+         if whrandom.randint( oretable[ resname ][ MINSKILL ], oretable[ resname ][ MAXSKILL ] ) < char.skill[ MINING ]:
+            skills.successharvest( char, veingem, oretable, resname, 1 ) # 1 - amount of ore
+         else:
+            socket.clilocmessage( 501869, "", YELLOW, NORMAL ) # You loosen some rocks but fail to find any usable ore.
+      else:
+         socket.clilocmessage( 501869, "", YELLOW, NORMAL ) # You loosen some rocks but fail to find any usable ore.
+   else:
+      socket.clilocmessage( 501869, "", YELLOW, NORMAL ) # You loosen some rocks but fail to find any usable ore.
+      
    char.deltag('nowmining')
    return OK
 
@@ -47,6 +70,7 @@ def getvein( socket, pos ):
    if len( gems ) < 1:
       gem = wolfpack.additem( 'ore_gem' )
       gem.settag( 'resourcecount', whrandom.randint( 10, 34 ) )
+      gem.settag( 'resname', 'iron' )
       gem.moveto( pos )
       gem.visible = 1
       gem.update()
