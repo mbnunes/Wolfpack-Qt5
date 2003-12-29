d4d536e5af2e84cc342bcffc7c959501136b3923
codex_ <codex_@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2003-12-29 13:21:31 +0000

    Aos modifiers tooltips

--- server/release/scripts/tooltip.py
+++ server/release/scripts/tooltip.py
@@ -12,7 +12,7 @@ import re
 
 # Register as a global script
 def onLoad():
-   wolfpack.registerglobal( EVENT_SHOWTOOLTIP, "tooltip" )
+	wolfpack.registerglobal( EVENT_SHOWTOOLTIP, "tooltip" )
 
 def onShowTooltip( sender, target, tooltip ):  
 
@@ -32,13 +32,6 @@ def onShowTooltip( sender, target, tooltip ):
 			weapon( target, tooltip )
 	                 
 		else:
-	#	   if target.name == '' or target.name == '#':
-   	#params = str( target.amount ) + "\t" + '#' + str( 0xF9060 + target.id )
-   	#print params
-   	#tooltip.add( 1050039, params )
-   #else:
-   	#tooltip.add( 1050039, str( target.amount ) + "\t" + target.getname() ) #$amount $name
-		
 			if target.name == '#' or target.name == '':
 				if target.amount > 1:
 					tooltip.add( 1050045, " \t#" + str( 0xF9060 + target.id ) + "\t`s: " + str( target.amount ) )
@@ -51,8 +44,7 @@ def onShowTooltip( sender, target, tooltip ):
 				else:
 					tooltip.add( 1050045, " \t" + name + "\t " )
 	
-			if target.hastag( "blessed" ):
-				tooltip.add( 1038021, "" ) # Blessed
+			modifiers( target, tooltip )
 		
 			if isspellbook( target ):
 				tooltip.add( 1042886, str( target.spellscount() ) )
@@ -65,66 +57,67 @@ def onShowTooltip( sender, target, tooltip ):
 	return 1
 
 def armor( target, tooltip ):
-   name = target.getname()
-   tooltip.add( 1050045, " \t" + name + "\t " ) #prefix name suffix
-   if (target.hastag( "blessed")):
-      tooltip.add( 1038021, "" ) # Blessed
-   tooltip.add( 1060448, "3" )                #Physical Resist 
-   tooltip.add( 1060447, "1" )                #Fire Resist
-   tooltip.add( 1060445, "1" )                #Cold Resist
-   tooltip.add( 1060449, "5" )                #Poison Resist
-   tooltip.add( 1060446, "3" )                #Energy Resist
-   if target.hastag( 'req_str' ):
-      tooltip.add( 1061170, str( target.gettag( 'req_str' ) ) )               #Strength Requirement
-   tooltip.add( 1060639, str( target.health ) + "\t" + str( target.maxhealth ) )           #Durability
+	name = target.getname()
+	tooltip.add( 1050045, " \t" + name + "\t " ) #prefix name suffix
+	modifiers( target, tooltip )
+	tooltip.add( 1060639, str( target.health ) + "\t" + str( target.maxhealth ) )           #Durability
 
 def container( target, tooltip ):
-   tooltip.add( 1050045, "Bag" )
-   tooltip.add( 1050044, "1000\t10000" )     #$count items, $weight stones
+	tooltip.add( 1050045, "Bag" )
+	tooltip.add( 1050044, "1000\t10000" )     #$count items, $weight stones
 
 def shield( target, tooltip ):
-   name = target.getname()
-   tooltip.add( 1050045, " \t" + name + "\t " )
-   if (target.hastag( "blessed")):
-      tooltip.add( 1038021, "" ) # Blessed
-   tooltip.add( 1060448, "1" )         #Physical resist
-   if target.hastag( 'req_str' ):
-      tooltip.add( 1061170, str( target.gettag( 'req_str' ) ) )               #Strength Requirement
-   tooltip.add( 1060639, str( target.health ) + "\t" + str( target.maxhealth ) )  #Durability
+	name = target.getname()
+	tooltip.add( 1050045, " \t" + name + "\t " )
+	modifiers( target, tooltip )
+	tooltip.add( 1060639, str( target.health ) + "\t" + str( target.maxhealth ) )  #Durability
 
 def hat( target, tooltip ):
-   name = target.getname()
-   tooltip.add( 1050045, " \t" + name + "\t " )
-   if (target.hastag( "blessed" )):
-      tooltip.add( 1038021, "" ) # Blessed
-   tooltip.add( 1060447, "5" )         #Fire Resist
-   tooltip.add( 1060445, "9" )         #Cold Resist
-   tooltip.add( 1060449, "5" )         #Poison Resist
-   tooltip.add( 1060446, "3" )         #Energy Resist
-   if target.hastag( 'req_str' ):
-      tooltip.add( 1061170, str( target.gettag( 'req_str' ) ) )               #Strength Requirement
-   tooltip.add( 1060639, str( target.health ) + "\t"  + str( target.maxhealth) )    #Durability
+	name = target.getname()
+	tooltip.add( 1050045, " \t" + name + "\t " )
+	modifiers( target, tooltip )
+	tooltip.add( 1060639, str( target.health ) + "\t"  + str( target.maxhealth) )    #Durability
 
 def weapon( target, tooltip ):
-   name = target.getname()
-   tooltip.add( 1050045, " \t" + name + "\t " )
-   if (target.hastag( "blessed" )):
-      tooltip.add( 1038021, "" ) # Blessed
-   tooltip.add( 1060403, "100" ) # Physical Damage
-   tooltip.add( 1061168, str( target.lodamage ) + " \t" + str(target.hidamage))# Weapon Damage
-   tooltip.add( 1061167, str( target.speed ))
-   if target.hastag( 'req_str' ):
-      tooltip.add( 1061170, str( target.gettag( 'req_str' ) ) )               #Strength Requirement
-   if ( target.twohanded ):
-      tooltip.add( 1061171, "" ) # two-handed
-   else:
-      tooltip.add( 1061824, "" ) # One-handed
-   if ( target.type ) == 1001 or ( target.type ) == 1002:
-      tooltip.add( 1061172, "" ) # Weapon Skill: Swordsmanship
-   if ( target.type ) == 1003 or ( target.type ) == 1004:
-      tooltip.add( 1061173, "" ) # Weapon Skill: Mace Fighting
-   if ( target.type ) == 1005:
-      tooltip.add( 1061174, "" ) #Weapon Skill: Fencing
-   if ( target.type ) == 1006 or ( target.type ) == 1007:
-      tooltip.add( 1061175, "" ) # Weapon Skill: Archery
-   tooltip.add( 1060639, str( target.health ) + "\t" + str( target.maxhealth ))
+	name = target.getname()
+	tooltip.add( 1050045, " \t" + name + "\t " )
+	modifiers( target, tooltip )
+	if ( target.twohanded ):
+		tooltip.add( 1061171, "" ) # two-handed
+	else:
+		tooltip.add( 1061824, "" ) # One-handed
+	if ( target.type ) == 1001 or ( target.type ) == 1002:
+		tooltip.add( 1061172, "" ) # Weapon Skill: Swordsmanship
+	if ( target.type ) == 1003 or ( target.type ) == 1004:
+		tooltip.add( 1061173, "" ) # Weapon Skill: Mace Fighting
+	if ( target.type ) == 1005:
+		tooltip.add( 1061174, "" ) #Weapon Skill: Fencing
+	if ( target.type ) == 1006 or ( target.type ) == 1007:
+		tooltip.add( 1061175, "" ) # Weapon Skill: Archery
+	tooltip.add( 1060639, str( target.health ) + "\t" + str( target.maxhealth ))
+
+def modifiers( target, tooltip ):
+	modifiers = {
+	"blessed" 	: [1038021,0],
+	"req_str" 	: [1061170,1],
+	"res_physical" 	: [1060448,1],
+	"res_fire"	: [1060447,1],
+	"res_cold"	: [1060445,1],
+	"res_poison"	: [1060449,1],
+	"res_energy"	: [1060446,1],
+	"dmg_physical" 	: [1060403,1],
+	"dmg_fire"	: [1060405,1],
+	"dmg_cold"	: [1060404,1],
+	"dmg_poison"	: [1060406,1],
+	"dmg_energy"	: [1060407,1]
+	}
+
+	for tagname in modifiers.keys():
+		if target.hastag( tagname ):
+			params = ""
+			if modifiers[ tagname ][1]:
+				params = str( target.gettag( tagname ) )
+			tooltip.add( modifiers[ tagname ][0], params )
+	
+
+
