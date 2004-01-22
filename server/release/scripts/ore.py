e12c305b40e78487bce4a25881c73d759eef0688
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-01-22 05:48:18 +0000

    I'll finish this when I get the chance, or someone can beat me to it :P Just wanted to add it so I don't forget.

---
+++ server/release/scripts/ore.py
@@ -0,0 +1,9 @@
+
+# I'll work on this later, this will be for smelting ore into ingots.
+# This will need to be mining skill based, same ratio as mining
+# for success. - Dreoth
+
+import wolfpack
+
+def onUse( char, ore ):
+	char.socket.sysmessage( 'Smelt Test' )
