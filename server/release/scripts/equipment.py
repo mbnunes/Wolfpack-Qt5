e52a8d23855ecae3ca7994101008d99b75b551ff
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-01-26 06:23:08 +0000

    This fixes a glitch where it wouldn't update the stats on Unequip.

--- server/release/scripts/equipment.py
+++ server/release/scripts/equipment.py
@@ -1,4 +1,7 @@
 
+import wolfpack
+from wolfpack.utilities import *
+
 # Check for Strength Requirements
 def onWearItem( player, wearer, item, layer ):
 	if item.hastag( 'req_str' ) and wearer.strength < int( item.gettag( 'req_str' ) ):
@@ -15,7 +18,7 @@ def onWearItem( player, wearer, item, layer ):
 			return 1
 		else:
 			player.message( 'You cannot wear that item, you seem not agile enough.' )
-			return 1			
+			return 1
 
 	if item.hastag( 'req_int' ) and wearer.intelligence < int( item.gettag( 'req_int' ) ):
 		if player != wearer:
@@ -23,27 +26,32 @@ def onWearItem( player, wearer, item, layer ):
 			return 1
 		else:
 			player.message( 'You cannot wear that item, you seem not smart enough.' )
-			return 1			
+			return 1
 			
 	return 0
 
 def onEquip( char, item, layer ):
-	# Boni?
+	# Bonus Strength
 	if item.hastag( 'boni_str' ):
 		char.strength = char.strength + int( item.gettag( 'boni_str' ) )
-		
+	# Bonus Dex
 	if item.hastag( 'boni_dex' ):
 		char.dexterity = char.dexterity + int( item.gettag( 'boni_dex' ) )
-		
+	# Bonus Int
 	if item.hastag( 'boni_int' ):
 		char.intelligence = char.intelligence + int( item.gettag( 'boni_int' ) )
-	
+	# Update Stats
+	char.updatestats()
+
 def onUnequip( char, item, layer ):
+	# Bonus Str
 	if item.hastag( 'boni_str' ):
 		char.strength = char.strength - int( item.gettag( 'boni_str' ) )
-		
+	# Bonus Dex
 	if item.hastag( 'boni_dex' ):
 		char.dexterity = char.dexterity - int( item.gettag( 'boni_dex' ) )
-		
+	# Bonus Int
 	if item.hastag( 'boni_int' ):
 		char.intelligence = char.intelligence - int( item.gettag( 'boni_int' ) )
+	# Update Stats
+	char.updatestats()
