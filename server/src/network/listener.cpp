dd6cf42272652424931d4f28360607cd69ce13ee
thiagocorrea <thiagocorrea@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-01-27 23:46:46 +0000

    Tries to reduce Linux down time in the event of a crash with socket linger option.

--- server/src/network/listener.cpp
+++ server/src/network/listener.cpp
@@ -70,10 +70,10 @@ cListener::~cListener() throw()
 
 void cListener::run() throw()
 {
+	listenningSocket.setAddressReusable( true );
 	listenningSocket.bind( static_cast<Q_UINT32>(0), _port );
 	listenningSocket.listen( 20 );
 	listenningSocket.setBlocking( false ); // or else it would take a while to join()
-	listenningSocket.setAddressReusable( true );
 
 	while ( !canceled() )
 	{
