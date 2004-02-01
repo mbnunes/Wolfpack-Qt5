b3048cbc6de5dcd0a2d027bbe39ac08853a2e86f
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-02-01 23:55:51 +0000

    Barber changes.

--- server/release/scripts/deeds/hairstyle.py
+++ server/release/scripts/deeds/hairstyle.py
@@ -15,49 +15,49 @@ def onUse(char, deed):
     
     gump.addResizeGump(100, 10, 0xA28, 400, 385)
     
-    #gump.addXmfHtmlGump(100, 25, 400, 35, 1013008, 0, 0)
-    #gump.addButton(175, 340, 0xFA5, 0xFA7, 0)
+    gump.addXmfHtmlGump(100, 25, 400, 35, 1013008, 0, 0)
+    gump.addButton(175, 340, 0xFA5, 0xFA7, 0)
     
-    #gump.addXmfHtmlGump(210, 342, 90, 35, 1011012, 0, 0)
+    gump.addXmfHtmlGump(210, 342, 90, 35, 1011012, 0, 0)
     
-    #gump.addResizeGump(220, 60, 0xA3C, 50, 50)
-    #gump.addResizeGump(220, 115, 0xA3C, 50, 50)
-    #gump.addResizeGump(220, 170, 0xA3C, 50, 50)
-    #gump.addResizeGump(220, 225, 0xA3C, 50, 50)
-    #gump.addResizeGump(425, 60, 0xA3C, 50, 50)
-    #gump.addResizeGump(425, 115, 0xA3C, 50, 50)
-    #gump.addResizeGump(425, 170, 0xA3C, 50, 50)
-    #gump.addResizeGump(425, 225, 0xA3C, 50, 50)
-    #gump.addResizeGump(425, 280, 0xA3C, 50, 50)
+    gump.addResizeGump(220, 60, 0xA3C, 50, 50)
+    gump.addResizeGump(220, 115, 0xA3C, 50, 50)
+    gump.addResizeGump(220, 170, 0xA3C, 50, 50)
+    gump.addResizeGump(220, 225, 0xA3C, 50, 50)
+    gump.addResizeGump(425, 60, 0xA3C, 50, 50)
+    gump.addResizeGump(425, 115, 0xA3C, 50, 50)
+    gump.addResizeGump(425, 170, 0xA3C, 50, 50)
+    gump.addResizeGump(425, 225, 0xA3C, 50, 50)
+    gump.addResizeGump(425, 280, 0xA3C, 50, 50)
     
-    #gump.addXmfHtmlGump( 150, 75, 80, 35, 1011052, 0, 0 ) # Short
-    #gump.addXmfHtmlGump( 150, 130, 80, 35, 1011053, 0, 0 ) # Long
-    #gump.addXmfHtmlGump( 150, 185, 80, 35, 1011054, 0, 0 ) # Ponytail
-    #gump.addXmfHtmlGump( 150, 240, 80, 35, 1011055, 0, 0 ) # Mohawk
-    #gump.addXmfHtmlGump( 355, 75, 80, 35, 1011047, 0, 0 ) # Pageboy
-    #gump.addXmfHtmlGump( 355, 130, 80, 35, 1011048, 0, 0 ) # Receding
-    #gump.addXmfHtmlGump( 355, 185, 80, 35, 1011049, 0, 0 ) # 2-tails
-    #gump.addXmfHtmlGump( 355, 240, 80, 35, 1011050, 0, 0 ) # Topknot
-    #gump.addXmfHtmlGump( 355, 295, 80, 35, 1011064, 0, 0 ) # Bald
+    gump.addXmfHtmlGump( 150, 75, 80, 35, 1011052, 0, 0 ) # Short
+    gump.addXmfHtmlGump( 150, 130, 80, 35, 1011053, 0, 0 ) # Long
+    gump.addXmfHtmlGump( 150, 185, 80, 35, 1011054, 0, 0 ) # Ponytail
+    gump.addXmfHtmlGump( 150, 240, 80, 35, 1011055, 0, 0 ) # Mohawk
+    gump.addXmfHtmlGump( 355, 75, 80, 35, 1011047, 0, 0 ) # Pageboy
+    gump.addXmfHtmlGump( 355, 130, 80, 35, 1011048, 0, 0 ) # Receding
+    gump.addXmfHtmlGump( 355, 185, 80, 35, 1011049, 0, 0 ) # 2-tails
+    gump.addXmfHtmlGump( 355, 240, 80, 35, 1011050, 0, 0 ) # Topknot
+    gump.addXmfHtmlGump( 355, 295, 80, 35, 1011064, 0, 0 ) # Bald
     
-    gump.addTilePic( 153, 20, 0xC60C )
-    #gump.addTilePic( 153, 65, 0xED24 )
-    #gump.addTilePic( 153, 120, 0xED1E )
-    #gump.addTilePic( 153, 185, 0xC60F )
-    #gump.addTilePic( 358, 18, 0xED26 )
-    #gump.addTilePic( 358, 75, 0xEDE5 )
-    #gump.addTilePic( 358, 120, 0xED23 )
-    #gump.addTilePic( 362, 190, 0xED29 )
+    gump.addGump( 153, 20, 0xC60C )
+    gump.addGump( 153, 65, 0xED24 )
+    gump.addGump( 153, 120, 0xED1E )
+    gump.addGump( 153, 185, 0xC60F )
+    gump.addGump( 358, 18, 0xED26 )
+    gump.addGump( 358, 75, 0xEDE5 )
+    gump.addGump( 358, 120, 0xED23 )
+    gump.addGump( 362, 190, 0xED29 )
     
-    #gump.addButton(118, 73, 0xFA5, 0xFA7, 2)
-    #gump.addButton(118, 128, 0xFA5, 0xFA7, 3)
-    #gump.addButton(118, 183, 0xFA5, 0xFA7, 4)
-    #gump.addButton(118, 238, 0xFA5, 0xFA7, 5)
-    #gump.addButton(323, 73, 0xFA5, 0xFA7, 6)
-    #gump.addButton(323, 128, 0xFA5, 0xFA7, 7)
-    #gump.addButton(323, 183, 0xFA5, 0xFA7, 8)
-    #gump.addButton(323, 238, 0xFA5, 0xFA7, 9)
-    #gump.addButton(323, 292, 0xFA5, 0xFA7, 1)
+    gump.addButton(118, 73, 0xFA5, 0xFA7, 2)
+    gump.addButton(118, 128, 0xFA5, 0xFA7, 3)
+    gump.addButton(118, 183, 0xFA5, 0xFA7, 4)
+    gump.addButton(118, 238, 0xFA5, 0xFA7, 5)
+    gump.addButton(323, 73, 0xFA5, 0xFA7, 6)
+    gump.addButton(323, 128, 0xFA5, 0xFA7, 7)
+    gump.addButton(323, 183, 0xFA5, 0xFA7, 8)
+    gump.addButton(323, 238, 0xFA5, 0xFA7, 9)
+    gump.addButton(323, 292, 0xFA5, 0xFA7, 1)
 
     gump.send(char)
 
--- server/release/scripts/wolfpack/gumps.py
+++ server/release/scripts/wolfpack/gumps.py
@@ -36,14 +36,14 @@ class cGump:
 			raise TypeError( "You passed an invalid socket." )
 
 		# Dump the gump data
-		#file = open( 'dump.txt', 'w' )
-		#file.write( "--------\nLayout:\n" )
-		#for line in self.layout:
-		#	file.write( line + "\n" )
-		#file.write( "--------\nText:\n" )
-		#for line in self.texts:
-		#	file.write( line + "\n" )
-		#file.close()
+		file = open( 'dump.txt', 'w' )
+		file.write( "--------\nLayout:\n" )
+		for line in self.layout:
+			file.write( line + "\n" )
+		file.write( "--------\nText:\n" )
+		for line in self.texts:
+			file.write( line + "\n" )
+		file.close()
 
 		if( self.noclose == 1 ):
 			self.layout.insert( 0, '{ noclose }' )
