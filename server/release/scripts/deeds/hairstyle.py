c23db52ffd0f17e9b4e7f46e9045f877d5d79221
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-02-02 02:11:37 +0000

    Fixed a problem.

--- server/release/scripts/deeds/hairstyle.py
+++ server/release/scripts/deeds/hairstyle.py
@@ -55,7 +55,7 @@ def response(char, args, response):
     deed.delete()
 
 def onUse(char, deed):
-  if char.id != 0x190 or char.id != 0x191:
+  if char.id != 0x190 and char.id != 0x191:
     char.socket.clilocmessage(1042298)
     return 1
 
