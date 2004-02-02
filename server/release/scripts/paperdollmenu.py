01dc28327ff110a1a3c53029f135b4641bbff3f9
thiagocorrea <thiagocorrea@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-02-02 04:59:51 +0000

    Implemented:
    - Paperdoll context menu entry
    - Random <saycolor>

---
+++ server/release/scripts/paperdollmenu.py
@@ -0,0 +1,15 @@
+#################################################################
+#   )      (\_     # WOLFPACK 13.0.0 Scripts                    #
+#  ((    _/{  "-;  # Created by: Correa                         #
+#   )).-' {{ ;'`   # Revised by:                                #
+#  ( (  ;._ \\ ctr # Last Modification:                         #
+#################################################################
+
+
+def onContextEntry( char, target, tag  ):
+
+    if( tag == 1 ):
+        if ( char.socket ):
+            char.socket.sendpaperdoll( target )
+    return 1
+
