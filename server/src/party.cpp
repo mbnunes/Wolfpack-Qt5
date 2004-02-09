fc8ede3c47de5f10da41e0e4e96239676ac8d982
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-02-09 20:48:25 +0000

    - Party System completed This includes but is not limited to: looting rights, member death notification, invitation timeout (10 seconds), sending status updates about members even if out of range, seeing hidden party members (grayed out), sending messages to single party members and to the whole party. Additionally, if a party member kills a creature, fame and karma are awarded to all party members who can see the creature. (Note: Fame and Karma are not split up between members, but awarded equally).
    - Fixed a possible crashbug (especially on worldsaves) related to baseid that could arise when definitions are reloaded.
    
    - Removed all hit area messages (you hit xxx in the groin).
    
    - Fixed floating damage indicators (the amount of damage you deal in red numbers over the victims head)
    
    - Fixed an issue with broken messages when items wear out and are destroyed.
    
    - Made parties scriptable trough python:
    access the party object via char.party (None for NPCs or if no party exists)
    properties: members, canidates, lootingallowed
    methods: tellsingle(source, target, message) and tellall(source, message)
    
    - Fixed an issue with players not being sent to other players when walking into their range. Cleaned up the sending code in walking.cpp and made it use cBaseChar::canSee to check whether the peer is visible or not.
    
    - Made screenwidth and screenheight sent by the client available via python (properties for the socket class: screenwidth, screenheight,) They default to 640 and 480.
    
    - Fixed reputation titles showing up on NPC paperdolls.

---
+++ server/src/party.cpp
@@ -0,0 +1,523 @@
+//==================================================================================
+//
+//      Wolfpack Emu (WP)
+//	UO Server Emulation Program
+//
+//	Copyright 1997, 98 by Marcus Rating (Cironian)
+//  Copyright 2001-2004 by holders identified in authors.txt
+//	This program is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation; either version 2 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program; if not, write to the Free Software
+//	Foundation, Inc., 59 Temple Palace - Suite 330, Boston, MA 02111-1307, USA.
+//
+//	* In addition to that license, if you are running this program or modified
+//	* versions of it on a public system you HAVE TO make the complete source of
+//	* the version used by you available or provide people with a location to
+//	* download it.
+//
+//
+//
+//	Wolfpack Homepage: http://wpdev.sf.net/
+//==================================================================================
+
+#include "party.h"
+#include "player.h"
+#include "npc.h"
+#include "world.h"
+#include "network/uosocket.h"
+
+cParty::cParty(P_PLAYER leader) {
+	leader_ = leader;
+	addMember(leader);
+}
+
+cParty::~cParty() {
+	for (P_PLAYER member = members_.first(); member; member = members_.next()) {
+		member->setParty(0);
+
+		if (member->socket()) {
+			cUOTxPartyRemoveMember updateparty;
+			updateparty.setSerial(member->serial());
+			member->socket()->send(&updateparty);
+			member->socket()->clilocMessage(1005449);
+		}
+	}
+	
+	while (canidates_.count() > 0) {
+		if (canidates_.first()->socket()) {
+			cUOTxPartyRemoveMember updateparty;
+			updateparty.setSerial(canidates_.first()->serial());
+			canidates_.first()->socket()->send(&updateparty);
+			canidates_.first()->socket()->clilocMessage(1005449);
+		}
+
+		canidates_.erase(canidates_.begin());
+	}
+}
+
+void cParty::addMember(P_PLAYER player, bool update) {
+	// Remove him from his old party
+	if (player->party()) {
+		player->party()->removeMember(player);
+	} else {
+		removeCanidate(player);
+	}
+
+	if (update) {
+		send(1008094, QString::null, player->name(), true);
+	}
+
+	members_.append(player);
+	player->setParty(this);
+
+	// Send the new memberlist to all members
+	if (update) {
+		this->update();
+
+		if (player != leader_ && player->socket()) {
+			player->socket()->clilocMessage(1005445);
+		}
+	}
+}
+
+void cParty::removeMember(P_PLAYER player, bool update) {
+	removeCanidate(player);
+	members_.remove(player);
+	player->setParty(0);
+
+	// Update the party
+	if (update) {
+		cUOTxPartyRemoveMember removemember;
+		removemember.setSerial(player->serial());
+		for (P_PLAYER member = members_.first(); member; member = members_.next()) {
+			removemember.addMember(member->serial());
+		}
+		send(&removemember);
+		
+		if (player->socket()) {
+			if (player == leader_) {
+				player->socket()->clilocMessage(1005456);
+			}
+
+			cUOTxPartyRemoveMember updateparty;
+			updateparty.setSerial(player->serial());
+			player->socket()->send(&updateparty);
+		}
+	}
+
+	// Check if the party can be disbanded
+	if (player == leader_ || (members_.count() <= 1 && canidates_.count() == 0)) {
+		if (player != leader_) {
+			if (leader_->socket()) {
+				leader_->socket()->clilocMessage(1005450);
+			}
+		}
+
+		delete this;
+	}
+}
+
+void cParty::removeCanidate(P_PLAYER player) {
+	canidates_.remove(player);
+}
+
+void cParty::addCanidate(P_PLAYER player) {
+	canidates_.append(player);
+}
+
+void cParty::update() {
+	cUOTxPartyUpdate update;
+
+	for (P_PLAYER player = members_.first(); player; player = members_.next()) {
+		update.addMember(player->serial());
+	}
+
+	send(&update);
+}
+
+void cParty::send(cUOPacket *packet) {
+	for (P_PLAYER player = members_.first(); player; player = members_.next()) {
+		if (player->socket()) {
+			player->socket()->send(packet);
+		}
+	}
+}
+
+void cParty::send(unsigned int message, const QString &params, const QString &affix, bool prepend) {
+	for (P_PLAYER player = members_.first(); player; player = members_.next()) {
+		if (player->socket()) {
+			player->socket()->clilocMessageAffix(message, params, affix, 0x3B2, 3, 0, false, prepend);			
+		}
+	}
+}
+
+void cParty::send(P_PLAYER from, const QString &message) {
+	cUOTxPartyTellMember tell;
+	tell.setSerial(from->serial());
+	tell.setText(message);
+
+	for (P_PLAYER player = members_.first(); player; player = members_.next()) {
+		if (player->socket()) {
+			player->socket()->send(&tell);
+		}
+	}
+}
+
+void cParty::send(P_PLAYER from, P_PLAYER target, const QString &message) {
+	cUOTxPartyTellMember tell;
+	tell.setSerial(from->serial());
+	tell.setText(message);
+
+	for (P_PLAYER player = members_.first(); player; player = members_.next()) {
+		if (player == target && player->socket()) {
+			player->socket()->send(&tell);
+		}
+	}	
+}
+
+void cParty::setLootingAllowed(P_PLAYER player, bool allowed) {
+	if (allowed) {
+		if (!lootingAllowed_.contains(player)) {
+			lootingAllowed_.append(player);
+		}
+	} else {
+		lootingAllowed_.remove(player);
+	}
+}
+
+/*!
+	\brief Temporary effect for canceling a party invitation after a given timeout.
+*/
+class cPartyCancelInvitation : public cTempEffect {
+protected:
+    SERIAL leader;
+
+public:
+	cPartyCancelInvitation(SERIAL leader, SERIAL player) {
+		this->leader = leader;
+		destSer = player;
+		setExpiretime_s(10);
+		serializable = false;
+		dispellable = false;
+		objectid = "cancelpartyinvitation";
+	}
+
+	/*!
+		If source is 0 and silent is true, nothing happens. Otherwise
+		the canidate is removed from the party without notification.
+	*/
+	void Dispel(P_CHAR source, bool silent) {		
+		P_PLAYER player = dynamic_cast<P_PLAYER>(World::instance()->findChar(destSer));
+		P_PLAYER leader = dynamic_cast<P_PLAYER>(World::instance()->findChar(this->leader));
+
+		if (leader && leader->party() && !silent) {
+			leader->party()->removeMember(player);
+		}
+	}
+
+	/*!
+		Remove the canidate from the party and notify him.
+	*/
+	void Expire() {
+		// Check if the player is still waiting for the party acceptance
+		P_PLAYER leader = dynamic_cast<P_PLAYER>(World::instance()->findChar(this->leader));
+		P_PLAYER player = dynamic_cast<P_PLAYER>(World::instance()->findChar(destSer));
+
+		// Only send a decline party invitation packet if the player really is 
+		// still invited to join the party
+		if (leader && leader->party() && player && player->socket() && leader->party()->canidates().contains(player)) {
+			player->socket()->log(LOG_TRACE, QString("Party invitation from '%1' timed out.\n").arg(leader->account()->login()));
+			leader->party()->removeMember(player); // This automatically resends and checks the party			
+		}	
+	}
+};
+
+/*!
+	\brief Target request for sending a party invitation to a given player.
+*/
+class cPartyInvitation : public cTargetRequest {
+protected:
+	SERIAL player;
+
+public:
+	cPartyInvitation(SERIAL player) {
+		this->player = player;
+	}
+
+	bool responsed(cUOSocket *socket, cUORxTarget *target) {
+		if (!isCharSerial(target->serial())) {
+			socket->clilocMessage(1005442);
+		} else {
+			P_PLAYER leader = dynamic_cast<P_PLAYER>(World::instance()->findChar(this->player));
+			P_CHAR character = World::instance()->findChar(target->serial());
+			P_NPC npc = dynamic_cast<P_NPC>(character);
+			P_PLAYER player = dynamic_cast<P_PLAYER>(character);
+
+			if (!leader || (leader->party() && leader->party()->leader() != leader)) {
+				socket->clilocMessage(1005453);
+			} else if (leader->party() && leader->party()->members().count() + leader->party()->canidates().count() >= 10) {
+				socket->clilocMessage(1008095);
+			// NPC targetted
+			} else if (npc) {
+				if (npc->isHuman()) {
+					socket->clilocMessage(1005443, 0, npc->saycolor(), 3, npc);
+				} else {
+					socket->clilocMessage(1005444);
+				}
+			} else if (leader == player) {
+				socket->clilocMessage(1005439);
+			} else if (player && player->party() && player->party() == leader->party()) {
+				socket->clilocMessage(1005440);
+			} else if (player && leader->party() && leader->party()->canidates().contains(player)) {
+				socket->clilocMessage(1005440);
+			} else if (player && player->party()) {
+				socket->clilocMessage(1005441);
+			} else if (player && player->socket()) {
+				if (!leader->party()) {
+					new cParty(leader);
+					leader->party()->update();
+				}
+
+				player->socket()->clilocMessageAffix(1008089, 0, leader->name(), 0x3b2, 3, 0, false, true);
+				leader->party()->addCanidate(player);
+				socket->log(LOG_TRACE, QString("Invited '%1' to join his party.\n").arg(player->account()->login()));
+
+				// Send a party invitation request
+				cUOTxPartyInvitation invitation;
+				invitation.setSerial(leader->serial());
+				player->socket()->send(&invitation);
+
+				// Attach a tempeffect that'll cancel the invitation after ten seconds
+				TempEffects::instance()->insert(new cPartyCancelInvitation(leader->serial(), player->serial()));
+			}
+		}
+
+		return true;
+	}
+};
+
+void cParty::handlePacket(cUOSocket *socket, cUOPacket *packet) {
+	unsigned char subcommand = (*packet)[5];
+	QString message;
+	P_PLAYER leader = 0;
+	P_PLAYER player = socket->player();
+
+	switch (subcommand) {
+		// Add a member to the party
+		case 1:
+			socket->clilocMessage(1005454);
+			socket->attachTarget(new cPartyInvitation(player->serial()));
+			break;
+
+		// Remove member from party.
+		case 2:
+			if (packet->size() == 10) {
+				P_PLAYER target = dynamic_cast<P_PLAYER>(World::instance()->findChar(packet->getInt(6)));
+
+				if (target && player->party() && target->party()) {
+					if (player->party() == target->party() && player->party()->leader() == player) {
+						socket->log(LOG_TRACE, QString("Removed '%1' from the party.\n").arg(target->account()->login()));
+						player->party()->removeMember(target);
+					} else if (target == player) {
+						socket->log(LOG_TRACE, QString("Left the party.\n").arg(player->account()->login()));
+						player->party()->removeMember(target);
+					}
+				}
+			}
+			break;
+
+		// Send a single party member a message
+		case 3:
+			if (player->party()) {				
+				P_PLAYER target = dynamic_cast<P_PLAYER>(World::instance()->findChar(packet->getInt(6)));
+				if (target) {
+					socket->log(LOG_TRACE, QString("Told '%1' in party '%2'.\n").arg(target->account()->login(), message));
+					QString message = packet->getUnicodeString(10, packet->size() - 10);
+					player->party()->send(player, target, message);
+				}
+			}
+			break;
+
+		// Send the whole party a message
+		case 4:
+			if (player->party()) {
+				QString message = packet->getUnicodeString(6, packet->size() - 6);
+				socket->log(LOG_TRACE, QString("Told the whole party: '%1'.\n").arg(message));
+				player->party()->send(player, message);				
+			}
+			break;
+
+		// Allow or Disallow my party from looting my corpse
+		case 6:
+			if (player->party() && packet->size() == 7) {
+				bool allowed = (*packet)[6] != 0;
+				player->party()->setLootingAllowed(player, allowed);
+				socket->clilocMessage(allowed ? 1005447 : 1005448);
+			}
+			break;
+		
+		// Accept party invitation
+		case 8:
+			TempEffects::instance()->dispel(player, 0, "cancelpartyinvitation", true, false);
+
+			leader = dynamic_cast<P_PLAYER>(World::instance()->findChar(packet->getInt(6)));
+			if (leader && leader->party() && leader->party()->canidates().contains(player)) {
+				leader->party()->addMember(player);
+				socket->log(QString("Accepted party invitation from '%1'.\n").arg(leader->account()->login()));
+			}
+			break;
+
+		// Decline party invitation
+		case 9:
+			TempEffects::instance()->dispel(player, 0, "cancelpartyinvitation", true, false);
+
+			leader = dynamic_cast<P_PLAYER>(World::instance()->findChar(packet->getInt(6)));
+			if (leader && leader->party() && leader->party()->canidates().contains(player)) {
+				leader->socket()->clilocMessageAffix(1008091, 0, player->name(), 0x3b2, 3, 0, false, true);
+				leader->party()->removeMember(player);
+				socket->clilocMessage(1008092);
+
+				socket->log(LOG_TRACE, QString("Declined party invitation from '%1'.\n").arg(leader->account()->login()));
+			}
+			break;
+
+		default:
+			message.sprintf("Receieved unknown party subcommand: 0x%02x", subcommand);
+			message	+= packet->dump(packet->uncompressed()) + "\n";
+			socket->log(LOG_WARNING, message);
+			break;
+	};
+}
+
+/*!
+	The python object structure for a party.
+*/
+struct wpParty {
+    PyObject_HEAD;
+	cParty *party;
+};
+
+static PyObject *wpParty_getAttr(wpParty *self, char *name);
+static int wpParty_setAttr(wpParty *self, char *name, PyObject *value);
+
+/*!
+	The python type object for the party type.
+*/
+PyTypeObject wpPartyType = {
+    PyObject_HEAD_INIT(NULL)
+    0,
+    "party",
+    sizeof(wpPartyType),
+    0,
+    wpDealloc,
+    0,
+    (getattrfunc)wpParty_getAttr,
+    (setattrfunc)wpParty_setAttr,
+};
+
+static PyObject *wpParty_tellsingle(wpParty *self, PyObject *args) {
+	P_CHAR source, target;
+	char *message;
+
+	if (PyArg_ParseTuple(args, "O&O&es:party.tellsingle(source, target, message)", PyConvertChar, &source, PyConvertChar, &target, "utf-8", &message)) {
+		P_PLAYER psource = dynamic_cast<P_PLAYER>(source);
+		P_PLAYER ptarget = dynamic_cast<P_PLAYER>(target);
+
+		if (psource && ptarget) {
+			self->party->send(psource, ptarget, QString::fromUtf8(message));
+		}
+
+		PyMem_Free(message);
+
+		return PyTrue;
+	}
+
+	return 0;
+}
+
+static PyObject *wpParty_tellall(wpParty *self, PyObject *args) {
+	P_CHAR source;
+	char *message;
+
+	if (PyArg_ParseTuple(args, "O&es:party.tellsingle(source, message)", PyConvertChar, &source, "utf-8", &message)) {
+		P_PLAYER psource = dynamic_cast<P_PLAYER>(source);
+
+		if (psource) {
+			self->party->send(psource, QString::fromUtf8(message));
+		}
+
+		PyMem_Free(message);
+		
+		return PyTrue;
+	}
+
+	return 0;
+}
+
+static PyMethodDef wpPartyMethods[] = {
+	{ "tellall", (getattrofunc)wpParty_tellall, METH_VARARGS, 0 },
+	{ "tellsingle", (getattrofunc)wpParty_tellsingle, METH_VARARGS, 0 },
+	{ 0, 0, 0, 0 }
+};
+
+static PyObject *wpParty_getAttr(wpParty *self, char *name) {
+	cParty *party = self->party;
+
+	if (!strcmp(name, "leader")) {
+		return party->leader()->getPyObject();
+	} else if (!strcmp(name, "members")) {
+		QPtrList<cPlayer> members = party->members();
+		PyObject *list = PyList_New(0);
+		for (P_PLAYER member = members.first(); member; member = members.next()) {
+			PyList_Append(list, member->getPyObject());
+		}
+		return list;
+	} else if (!strcmp(name, "canidates")) {
+		QPtrList<cPlayer> canidates = party->canidates();
+		PyObject *list = PyList_New(0);
+		for (P_PLAYER canidate = canidates.first(); canidate; canidate = canidates.next()) {
+			PyList_Append(list, canidate->getPyObject());
+		}
+		return list;
+	} else if (!strcmp(name, "lootingallowed")) {
+		QPtrList<cPlayer> lootlist = party->lootingAllowed();
+		PyObject *list = PyList_New(0);
+		for (P_PLAYER member = lootlist.first(); member; member = lootlist.next()) {
+			PyList_Append(list, member->getPyObject());
+		}
+		return list;
+	}
+
+	return Py_FindMethod(wpPartyMethods, (PyObject*)self, name);
+}
+
+static int wpParty_setAttr(wpParty *self, char *name, PyObject *value) {
+	return 0;
+}
+
+const char *cParty::className() const {
+	return "party";
+}
+
+PyObject *cParty::getPyObject() {
+	wpParty *returnVal = PyObject_New(wpParty, &wpPartyType);
+	returnVal->party = this;
+	return (PyObject*)returnVal;
+}
+
+bool cParty::implements(const QString &name) const {
+	if (name == className()) {
+		return true;
+	} else {
+		return cPythonScriptable::implements(name);
+	}
+}
---
+++ server/src/party.h
@@ -0,0 +1,159 @@
+//==================================================================================
+//
+//      Wolfpack Emu (WP)
+//	UO Server Emulation Program
+//
+//	Copyright 1997, 98 by Marcus Rating (Cironian)
+//  Copyright 2001-2004 by holders identified in authors.txt
+//	This program is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation; either version 2 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program; if not, write to the Free Software
+//	Foundation, Inc., 59 Temple Palace - Suite 330, Boston, MA 02111-1307, USA.
+//
+//	* In addition to that license, if you are running this program or modified
+//	* versions of it on a public system you HAVE TO make the complete source of
+//	* the version used by you available or provide people with a location to
+//	* download it.
+//
+//
+//
+//	Wolfpack Homepage: http://wpdev.sf.net/
+//==================================================================================
+
+#if !defined(__PARTY_H__)
+#define __PARTY_H__
+
+#include "pythonscript.h"
+#include "typedefs.h"
+#include <qptrlist.h>
+#include <qstring.h>
+
+/*!
+	\brief This class encapsulates a party.
+*/
+class cParty : public cPythonScriptable {
+protected:
+	P_PLAYER leader_;
+	QPtrList<cPlayer> members_;
+	QPtrList<cPlayer> canidates_;
+	QPtrList<cPlayer> lootingAllowed_;
+
+public:
+	/*!
+		\brief Create a new party.
+	*/
+	cParty(P_PLAYER leader);
+
+	/*!
+		\brief Disband the party.
+	*/
+	~cParty();
+
+	/*!
+		\brief Add the \a player to this party and remove him from his old.
+	*/
+	void addMember(P_PLAYER player, bool update = true);
+
+	/*!
+		\brief Remove the \a player from this party.
+	*/
+	void removeMember(P_PLAYER player, bool update = true);
+
+	/*!
+		\brief Adds a player to the list of canidates for this party.
+	*/
+	void addCanidate(P_PLAYER player);
+
+	/*!
+		\brief Removes a player from the list of canidates for this party.
+	*/
+	void removeCanidate(P_PLAYER player);
+
+	/*!
+		\returns The leader of this party.
+	*/
+	inline P_PLAYER leader() const {
+		return leader_;
+	}
+
+	/*!
+		\returns The members of this party.
+	*/
+	inline QPtrList<cPlayer> members() const {
+		return members_;
+	}
+
+	/*!
+		\returns The canidates for this party.
+	*/
+	inline QPtrList<cPlayer> canidates() const {
+		return canidates_;
+	}
+
+	/*!
+		\returns A list of players whose corpses may be looted.
+	*/
+	inline QPtrList<cPlayer> lootingAllowed() const {
+		return lootingAllowed_;
+	}
+
+	/*!
+		\brief Allows or disallows looting the corpse of a member of this party.
+		\param player The affected player.
+		\param allowed Whether looting the corpse is allowed or not.
+	*/
+	void setLootingAllowed(P_PLAYER player, bool allowed);
+    
+	/*!
+		\brief Sends an updated list of party members to all members.
+	*/
+	void update();
+
+	/*!
+		\brief Sends a packet to all party members.
+	*/
+	void send(cUOPacket *packet);
+
+	/*!
+		\brief Sends a localized message to all party members.
+	*/
+	void send(unsigned int message, const QString &params = QString::null, const QString &affix = QString::null, bool prepend = false);
+
+	/*!
+		\brief Sends a public message to the party.
+		\param from The source of the message.
+		\param message The text of the message.
+	*/
+	void send(P_PLAYER from, const QString &message);
+
+	/*!
+		\brief Sends a private message to a party member.
+		\param from The source of this message.
+		\param to The target of this message.
+		\param message The text of this message.
+	*/
+	void send(P_PLAYER from, P_PLAYER target, const QString &message);
+
+	/*!
+		\brief Processes a party packet sent by the specified player.
+		\param socket The socket this packet came from.
+		\param packet The packet that was sent.
+	*/
+	static void handlePacket(cUOSocket *socket, cUOPacket *packet);
+
+	// Inherited methods from cPythonScriptable
+	const char *className() const;
+	PyObject *getPyObject();
+	bool implements(const QString &name) const;
+};
+
+#endif
