4bf1b1b5f48881da14028fc83c26bd0d25be497d
aszlig <aszlig@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-02-17 20:02:36 +0000

    used a simple regexp to do line indenting

--- server/release/scripts/tooltip.py
+++ server/release/scripts/tooltip.py
@@ -14,33 +14,33 @@ import re
 def onLoad():
 	wolfpack.registerglobal( EVENT_SHOWTOOLTIP, "tooltip" )
 
-	def onShowTooltip( sender, target, tooltip ):  
+def onShowTooltip( sender, target, tooltip ):  
 	if target.isitem():
 		name = target.getname()
-	
+
 		if target.id == 0x2006 and len(target.name) > 0:
-		notoriety = 0x00
-	
-		if target.hastag('notoriety'):
-			notoriety = int(target.gettag('notoriety'))
-	
-		color = '#FFFFFF'
-	
-		# 0x01 Blue, 0x02 Green, 0x03 Grey, 0x05 Orange, 0x06 Red
-		colors = {
-			0x01 : '#00FFFF',
-			0x02 : '#10d010',
-			0x03 : '#d0d0d0',
-			0x04 : '#d0d0d0',
-			0x05 : '#ff9900',
-			0x06 : '#d01010'
-		}
-	
-		if not colors.has_key(notoriety):      
-			tooltip.add(1050045, " \tThe Remains Of \t" + target.name)
-		else:
-			tooltip.add(1050045, '<basefont color="%s">' % colors[notoriety] + " \tThe Remains Of \t<h3>" + target.name)
-	
+			notoriety = 0x00
+
+			if target.hastag('notoriety'):
+				notoriety = int(target.gettag('notoriety'))
+
+			color = '#FFFFFF'
+
+			# 0x01 Blue, 0x02 Green, 0x03 Grey, 0x05 Orange, 0x06 Red
+			colors = {
+				0x01 : '#00FFFF',
+				0x02 : '#10d010',
+				0x03 : '#d0d0d0',
+				0x04 : '#d0d0d0',
+				0x05 : '#ff9900',
+				0x06 : '#d01010'
+			}
+
+			if not colors.has_key(notoriety):      
+				tooltip.add(1050045, " \tThe Remains Of \t" + target.name)
+			else:
+				tooltip.add(1050045, '<basefont color="%s">' % colors[notoriety] + " \tThe Remains Of \t<h3>" + target.name)
+
 		elif isarmor ( target ):
 			armor( target, tooltip )
 		
@@ -49,73 +49,73 @@ def onLoad():
 		
 		elif isshield( target ):
 			shield( target, tooltip )
-			
+					 
 		elif isweapon( target ):
 			weapon( target, tooltip )
-					
+									 
 		else:
-		if target.name == '#' or target.name == '':
-			labelname = '#' + str(0xf9060 + target.id)
-		else:
-			labelname = target.name
-	
-		if target.amount > 1:
-			tooltip.add(1050039, str(target.amount) + "\t" + labelname)
-		else:
-			tooltip.add(1042971, labelname)
-	
-		if target.visible == 0 and sender.gm:
-			tooltip.add(3000507, "")
-		
-		modifiers( target, tooltip )
-		
-		# If the character is a gm and the targetted item has a lock, display the lock id
-		if sender.gm and 'lock' in target.events:
-			lock = 'None'
-			
-			if target.hastag('lock'):
-				lock = str(target.gettag('lock'))
-			
-			if target.hastag('locked') and int(target.gettag('locked')) != 0:
-			suffix = '<br>Locked'
+			if target.name == '#' or target.name == '':
+				labelname = '#' + str(0xf9060 + target.id)
 			else:
-			suffix = '<br>Unlocked'
-	
-			tooltip.add(1050045, " \tLock: " + lock + suffix + "\t ")
-		
-		if isspellbook( target ):
-			tooltip.add(1042886, str(target.spellscount()))
+				labelname = target.name
+
+			if target.amount > 1:
+				tooltip.add(1050039, str(target.amount) + "\t" + labelname)
+			else:
+				tooltip.add(1042971, labelname)
+
+			if target.visible == 0 and sender.gm:
+					tooltip.add(3000507, "")
+			
+			modifiers( target, tooltip )
+			
+			# If the character is a gm and the targetted item has a lock, display the lock id
+			if sender.gm and 'lock' in target.events:
+				lock = 'None'
+				
+				if target.hastag('lock'):
+					lock = str(target.gettag('lock'))
+				
+				if target.hastag('locked') and int(target.gettag('locked')) != 0:
+					suffix = '<br>Locked'
+				else:
+					suffix = '<br>Unlocked'
+
+				tooltip.add(1050045, " \tLock: " + lock + suffix + "\t ")
+			
+			if isspellbook( target ):
+				tooltip.add(1042886, str(target.spellscount()))
 	else:
 		affix = []
-	
+
 		if target.npc and len(target.title) > 0:
-		affix.append(target.title)
-	
+			affix.append(target.title)
+
 		if target.frozen:
-		affix.append('(frozen)')
-	
+			affix.append('(frozen)')
+
 		tooltip.add(1050045, " \t%s\t %s" % (target.name, " ".join(affix)))
 	
 		# Tame = 502006
 		if target.tamed:
-		tooltip.add(502006, "")
-	
+			tooltip.add(502006, "")
+
 		additional = []
-	
+
 		# These are only available to GMs
 		if sender.gm:
-		if target.invulnerable:
-			additional.append('(invulnerable)')
-			
-		if target.iscriminal():
-			additional.append('(criminal)')
+			if target.invulnerable:
+				additional.append('(invulnerable)')
+				
+			if target.iscriminal():
+				additional.append('(criminal)')
 	
-		if target.ismurderer():
-			additional.append('(murderer)')
-	
+			if target.ismurderer():
+				additional.append('(murderer)')
+
 		if len(additional) > 0:
-		tooltip.add(1042971, "<br>".join(additional))
-	
+			tooltip.add(1042971, "<br>".join(additional))
+
 	tooltip.send(sender)
 	return 1
 
@@ -161,26 +161,26 @@ def weapon( target, tooltip ):
 
 def modifiers( target, tooltip ):
 	modifiers = {
-	"blessed" 	: [1038021,0],
-	"req_str" 	: [1061170,1],
-	"aos_req_str"   : [1061170,1],
-	"boni_dex"	: [1060409,1],
-	"aos_boni_dex"	: [1060409,1],
-	"boni_int"	: [1060432,1],
-	"aos_boni_int"	: [1060432,1],
-	"boni_str"	: [1060485,1],
-	"aos_boni_str"	: [1060485,1],
-	"res_physical" 	: [1060448,1],
-	"res_fire"	: [1060447,1],
-	"res_cold"	: [1060445,1],
-	"res_poison"	: [1060449,1],
-	"res_energy"	: [1060446,1],
-	"dmg_physical" 	: [1060403,1],
-	"dmg_fire"	: [1060405,1],
-	"dmg_cold"	: [1060404,1],
-	"dmg_poison"	: [1060406,1],
-	"dmg_energy"	: [1060407,1],
-  "remaining_uses" : [1060584,1],
+		"blessed" 	: [1038021,0],
+		"req_str" 	: [1061170,1],
+		"aos_req_str"   : [1061170,1],
+		"boni_dex"	: [1060409,1],
+		"aos_boni_dex"	: [1060409,1],
+		"boni_int"	: [1060432,1],
+		"aos_boni_int"	: [1060432,1],
+		"boni_str"	: [1060485,1],
+		"aos_boni_str"	: [1060485,1],
+		"res_physical" 	: [1060448,1],
+		"res_fire"	: [1060447,1],
+		"res_cold"	: [1060445,1],
+		"res_poison"	: [1060449,1],
+		"res_energy"	: [1060446,1],
+		"dmg_physical" 	: [1060403,1],
+		"dmg_fire"	: [1060405,1],
+		"dmg_cold"	: [1060404,1],
+		"dmg_poison"	: [1060406,1],
+		"dmg_energy"	: [1060407,1],
+		"remaining_uses" : [1060584,1],
 	}
 
 	for tagname in modifiers.keys():
