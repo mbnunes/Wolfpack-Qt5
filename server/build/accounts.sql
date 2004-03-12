21defe482bd920461d5649bf1a3f60f7c50573f0
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-03-12 07:55:51 +0000

    Set the max length for account names to the client limit. Set the max password length to the md5 length.

--- server/build/accounts.sql
+++ server/build/accounts.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `accounts` (
-  `login` varchar(255) NOT NULL default '',
-  `password` varchar(255) NOT NULL default '',
+  `login` varchar(16) NOT NULL default '',
+  `password` varchar(32) NOT NULL default '',
   `flags` int NOT NULL default '0',
   `acl` varchar(255) NOT NULL default 'player',
   `lastlogin` int NOT NULL default '0',
