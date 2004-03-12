028950a4757e11d3f45e2ddc1db3e45840440cb7
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-03-12 16:33:41 +0000

    There, mysql checking.

--- server/release/scripts/mysql_optimize_db.py
+++ server/release/scripts/mysql_optimize_db.py
@@ -24,39 +24,55 @@ from wolfpack.database import *
 database = wolfpack.database
 log = wolfpack.console.log
 
-time = 24 * 3600000 # Value * Hours
+accountsdriver = database.driver( database.ACCOUNTS )
+worlddriver = database.driver( database.WORLD )
+
+time = 4 * 3600000 # Value * Hours
 
 #optimizequery = "OPTIMIZE TABLE `accounts` , `boats` , `boats_itemids` , `boats_itemoffsets` , `bookpages` , `books` , `characters` , `corpses` , `corpses_equipment` , `effects` , `effects_properties` , `guilds` , `guilds_canidates` , `guilds_members` , `houses` , `items` , `multis` , `multis_bans` , `multis_friends` , `npcs` , `players` , `settings` , `skills` , `tags` , `uobject` , `uobjectmap` "
 
 def onLoad():
-	wolfpack.addtimer( time, "mysql_optimize_db.timer", [] )
-	wolfpack.registercommand( "optimizedb", cmdoptimizedb )
+	if accountsdriver == 'mysql' or worlddriver == 'mysql':
+		wolfpack.addtimer( time, "mysql_optimize_db.timer", [] )
+		wolfpack.registercommand( "optimizedb", cmdoptimizedb )
 
 def cmdoptimizedb( socket, command, arguments ):
-	socket.sysmessage( "Optimizing the database.", GRAY )
-	optimize_db()
-	socket.sysmessage( "Done!", GRAY )
-	return
+	if accountsdriver == 'mysql' or worlddriver == 'mysql':
+		socket.sysmessage( "Optimizing the database.", GRAY )
+		optimize_db()
+		socket.sysmessage( "Done!", GRAY )
+		return
 
 def timer( timer, args ):
-	# Optimize and restart timer
-	optimize_db()
-	wolfpack.addtimer( time, "custom.shard_status.timer", [] )
-	return
+	if accountsdriver == 'mysql' or worlddriver == 'mysql':
+		# Optimize and restart timer
+		optimize_db()
+		wolfpack.addtimer( time, "custom.shard_status.timer", [] )
+		return
 
 def optimize_db():
-	try:
-		log( LOG_MESSAGE, "Optimizing accounts and world databases..." )
-		database.open( wolfpack.database.ACCOUNTS )
-		database.execute( "OPTIMIZE TABLE `accounts`" )
-		database.close()
-		log( LOG_MESSAGE, "Optimized accounts database!" )
-		database.open( wolfpack.database.WORLD )
-		database.execute( "OPTIMIZE TABLE `boats`, `boats_itemids`, `boats_itemoffsets`,`bookpages`,`books`, `characters`, `corpses`, `corpses_equipment`, `effects`, `effects_properties`, `guilds`, `guilds_canidates`, `guilds_members`, `houses`, `items`, `multis`, `multis_bans`, `multis_friends`, `npcs`, `players`, `settings`, `skills`, `tags`, `uobject`, `uobjectmap` " )
-		database.close()
-		log( LOG_MESSAGE, "Optimized world database!" )
-	except:
-		log( LOG_MESSAGE, " Performing world optimize failed." )
-
+	if accountsdriver == 'mysql' or worlddriver == 'mysql':
+		try:
+			# What are we optimizing
+			if accountsdriver == 'mysql' and worlddriver == 'mysql':
+				log( LOG_MESSAGE, "Optimizing accounts and world databases..." )
+			elif accountsdriver == 'mysql' or worlddriver == 'mysql':
+				if accountsdriver == 'mysql':
+					log( LOG_MESSAGE, "Optimizing accounts database..." )
+				elif worlddriver == 'mysql':
+					log( LOG_MESSAGE, "Optimizing world database..." )
+			# Time to optimize
+			if accountsdriver == 'mysql':
+				database.open( wolfpack.database.ACCOUNTS )
+				database.execute( "OPTIMIZE TABLE `accounts`" )
+				database.close()
+				log( LOG_MESSAGE, "Optimized accounts database!" )
+			if worlddriver == 'mysql':
+				database.open( wolfpack.database.WORLD )
+				database.execute( "OPTIMIZE TABLE `boats`, `boats_itemids`, `boats_itemoffsets`,`bookpages`,`books`, `characters`, `corpses`, `corpses_equipment`, `effects`, `effects_properties`, `guilds`, `guilds_canidates`, `guilds_members`, `houses`, `items`, `multis`, `multis_bans`, `multis_friends`, `npcs`, `players`, `settings`, `skills`, `tags`, `uobject`, `uobjectmap` " )
+				database.close()
+				log( LOG_MESSAGE, "Optimized world database!" )
+		except:
+			log( LOG_MESSAGE, " Performing world optimize failed." )
 	return
 
