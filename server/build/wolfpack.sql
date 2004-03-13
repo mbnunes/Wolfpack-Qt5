a01397c70329cfbd488de9fe6d661ac5dd72a9f1
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-03-13 02:09:54 +0000

    Statcaps.

--- server/build/wolfpack.sql
+++ server/build/wolfpack.sql
@@ -195,12 +195,13 @@ CREATE TABLE `players` (
 	`visualrange` tinyint(3)  NOT NULL default '0',
 	`profile` longtext,
 	`fixedlight` tinyint(3)  NOT NULL default '0',
-	`strcap` tinyint(4)  NOT NULL default '100',
-	`dexcap` tinyint(4)  NOT NULL default '100',
-	`intcap` tinyint(4)  NOT NULL default '100',
+	`strcap` tinyint(4)  NOT NULL default '125',
+	`dexcap` tinyint(4)  NOT NULL default '125',
+	`intcap` tinyint(4)  NOT NULL default '125',
 	`strlock` tinyint(4)  NOT NULL default '0',
 	`dexlock` tinyint(4)  NOT NULL default '0',
 	`intlock` tinyint(4)  NOT NULL default '0',
+  `statcap` tinyint(4)  NOT NULL default '225',
 	PRIMARY KEY (`serial`)
 );
 
