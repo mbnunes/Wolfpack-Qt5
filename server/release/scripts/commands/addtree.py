fbe037f53240f918b280dac3ec1a9066da96d415
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-04-08 21:26:36 +0000

    Works better, though the client sucks at handling the itemtarget thing.

--- server/release/scripts/commands/addtree.py
+++ server/release/scripts/commands/addtree.py
@@ -23,46 +23,80 @@ from random import randint
 		random places a random normal tree.
 	\notes Green leaves will be added ontop of the tree.
 """
+# Please leave the grayed out indexes for future reference incase itemtarget works better with multiple items.
 
-
-treeindex = ['cca','ccb','ccc','ccd','cd0','cd3','cd6','cda','cdd','ce0','ce3','ce6','cf8','cfe','d01']
+treeindex = ['cca','ccb','ccc','ccd','cd0','cd3','cd6','cda','cdd','ce0','ce3','ce6','cf8','cfe','d01', 'd94', 'd98', 'd9c', 'da0', 'da4', 'da8']
 
 jungleindex = \
 {
-	'jungle1': ['jungle1'],
-	'jungle2': ['jungle2'],
-	'jungle3': ['jungle2'],
-	'jungle4': ['jungle3'],
-	'jungle5': ['jungle4'],
-	'jungle6': ['jungle5'],
-	'jungle7': ['jungle6']
+	'jungle1': ['jungle1', [[hex2dec(0xd43), 0, 0, 0, 0]]],
+	'jungle2': ['jungle2', [[hex2dec(0xd43), 0, 0, 0, 0]]],
+	'jungle3': ['jungle2', [[hex2dec(0xd59), 0, 0, 0, 0]]],
+	'jungle4': ['jungle3', [[hex2dec(0xd70), 0, 0, 0, 0]]],
+	'jungle5': ['jungle4', [[hex2dec(0xd70), 0, 0, 0, 0]]],
+	'jungle6': ['jungle5', [[hex2dec(0xd85), 0, 0, 0, 0]]],
+	'jungle7': ['jungle6', [[hex2dec(0xd85), 0, 0, 0, 0]]]
 }
-
+'''
 trees = \
 {
-	'cca': ['cca', 'cce'],
-	'ccb': ['ccb', 'cce'],
-	'ccc': ['ccc', 'cce'],
-	'ccd': ['ccd', 'cce'],
-	'cd0': ['cd0', 'cd1'],
-	'cd3': ['cd3', 'cd4'],
-	'cd6': ['cd6', 'cd7'],
-	'cd8': ['cd8', 'cd0'],
-	'cda': ['cda', 'cdb'],
-	'cdd': ['cdd', 'cde'],
-	'ce0': ['ce0', 'ce1'],
-	'ce3': ['ce3', 'ce4'],
-	'ce6': ['ce6', 'ce7'],
-	'cf8': ['cf8', 'cf9'], # Cypress
-	'cfe': ['cfe', 'cff'], # Cypress
-	'd01': ['d01', 'd02'], # Cypress
-	'd94': ['d94', 'd95'],
-	'd98': ['d98', 'd99'],
-	'd9c': ['d9c', 'd9d'],
-	'da0': ['da0', 'da1'],
-	'da4': ['da4', 'da5'],
-	'da8': ['da8', 'da9']
+	'cca': ['cca', 'cce', [[hex2dec(0xcca), 0, 0, 0, 0],[hex2dec(0xcce), 0, 0, 0, 0]] ],
+	'ccb': ['ccb', 'cce', [[hex2dec(0xccb), 0, 0, 0, 0],[hex2dec(0xcce), 0, 0, 0, 0]] ],
+	'ccc': ['ccc', 'cce', [[hex2dec(0xccc), 0, 0, 0, 0],[hex2dec(0xcce), 0, 0, 0, 0]] ],
+	'ccd': ['ccd', 'cce', [[hex2dec(0xccd), 0, 0, 0, 0],[hex2dec(0xcce), 0, 0, 0, 0]] ],
+	'cd0': ['cd0', 'cd1', [[hex2dec(0xcd0), 0, 0, 0, 0],[hex2dec(0xcd1), 0, 0, 0, 0]] ],
+	'cd3': ['cd3', 'cd4', [[hex2dec(0xcd3), 0, 0, 0, 0],[hex2dec(0xcd4), 0, 0, 0, 0]] ],
+	'cd6': ['cd6', 'cd7', [[hex2dec(0xcd6), 0, 0, 0, 0],[hex2dec(0xcd7), 0, 0, 0, 0]] ],
+	'cd8': ['cd8', 'cd0', [[hex2dec(0xcd8), 0, 0, 0, 0],[hex2dec(0xcd0), 0, 0, 0, 0]] ],
+	'cda': ['cda', 'cdb', [[hex2dec(0xcda), 0, 0, 0, 0],[hex2dec(0xcdb), 0, 0, 0, 0]] ],
+	'cdd': ['cdd', 'cde', [[hex2dec(0xcdd), 0, 0, 0, 0],[hex2dec(0xcde), 0, 0, 0, 0]] ],
+	'ce0': ['ce0', 'ce1', [[hex2dec(0xce0), 0, 0, 0, 0],[hex2dec(0xce1), 0, 0, 0, 0]] ],
+	'ce3': ['ce3', 'ce4', [[hex2dec(0xce3), 0, 0, 0, 0],[hex2dec(0xce4), 0, 0, 0, 0]] ],
+	'ce6': ['ce6', 'ce7', [[hex2dec(0xce6), 0, 0, 0, 0],[hex2dec(0xce7), 0, 0, 0, 0]] ],
+	'cf8': ['cf8', 'cf9', [[hex2dec(0xcf8), 0, 0, 0, 0],[hex2dec(0xcf9), 0, 0, 0, 0]] ], # Cypress
+	'cfe': ['cfe', 'cff', [[hex2dec(0xcfe), 0, 0, 0, 0],[hex2dec(0xcff), 0, 0, 0, 0]] ], # Cypress
+	'd01': ['d01', 'd02', [[hex2dec(0xd01), 0, 0, 0, 0],[hex2dec(0xd02), 0, 0, 0, 0]] ], # Cypress
+	'd94': ['d94', 'd95', [[hex2dec(0xd94), 0, 0, 0, 0],[hex2dec(0xd95), 0, 0, 0, 0]] ],
+	'd98': ['d98', 'd99', [[hex2dec(0xd98), 0, 0, 0, 0],[hex2dec(0xd99), 0, 0, 0, 0]] ],
+	'd9c': ['d9c', 'd9d', [[hex2dec(0xd9c), 0, 0, 0, 0],[hex2dec(0xd9d), 0, 0, 0, 0]] ],
+	'da0': ['da0', 'da1', [[hex2dec(0xda0), 0, 0, 0, 0],[hex2dec(0xda1), 0, 0, 0, 0]] ],
+	'da4': ['da4', 'da5', [[hex2dec(0xda4), 0, 0, 0, 0],[hex2dec(0xda5), 0, 0, 0, 0]] ],
+	'da8': ['da8', 'da9', [[hex2dec(0xda8), 0, 0, 0, 0],[hex2dec(0xda9), 0, 0, 0, 0]] ]
 }
+'''
+trees = \
+{
+	'cca': ['cca', 'cce', [[hex2dec(0xcca), 0, 0, 0, 0]] ],
+	'ccb': ['ccb', 'cce', [[hex2dec(0xccb), 0, 0, 0, 0]] ],
+	'ccc': ['ccc', 'cce', [[hex2dec(0xccc), 0, 0, 0, 0]] ],
+	'ccd': ['ccd', 'cce', [[hex2dec(0xccd), 0, 0, 0, 0]] ],
+	'cd0': ['cd0', 'cd1', [[hex2dec(0xcd0), 0, 0, 0, 0]] ],
+	'cd3': ['cd3', 'cd4', [[hex2dec(0xcd3), 0, 0, 0, 0]] ],
+	'cd6': ['cd6', 'cd7', [[hex2dec(0xcd6), 0, 0, 0, 0]] ],
+	'cd8': ['cd8', 'cd0', [[hex2dec(0xcd8), 0, 0, 0, 0]] ],
+	'cda': ['cda', 'cdb', [[hex2dec(0xcda), 0, 0, 0, 0]] ],
+	'cdd': ['cdd', 'cde', [[hex2dec(0xcdd), 0, 0, 0, 0]] ],
+	'ce0': ['ce0', 'ce1', [[hex2dec(0xce0), 0, 0, 0, 0]] ],
+	'ce3': ['ce3', 'ce4', [[hex2dec(0xce3), 0, 0, 0, 0]] ],
+	'ce6': ['ce6', 'ce7', [[hex2dec(0xce6), 0, 0, 0, 0]] ],
+	'cf8': ['cf8', 'cf9', [[hex2dec(0xcf8), 0, 0, 0, 0]] ], # Cypress
+	'cfe': ['cfe', 'cff', [[hex2dec(0xcfe), 0, 0, 0, 0]] ], # Cypress
+	'd01': ['d01', 'd02', [[hex2dec(0xd01), 0, 0, 0, 0]] ], # Cypress
+	'd94': ['d94', 'd95', [[hex2dec(0xd94), 0, 0, 0, 0]] ],
+	'd98': ['d98', 'd99', [[hex2dec(0xd98), 0, 0, 0, 0]] ],
+	'd9c': ['d9c', 'd9d', [[hex2dec(0xd9c), 0, 0, 0, 0]] ],
+	'da0': ['da0', 'da1', [[hex2dec(0xda0), 0, 0, 0, 0]] ],
+	'da4': ['da4', 'da5', [[hex2dec(0xda4), 0, 0, 0, 0]] ],
+	'da8': ['da8', 'da9', [[hex2dec(0xda8), 0, 0, 0, 0]] ]
+}
+'''
+yewindex = [ [hex2dec(0x12b6), -4, 4, 0, 0], [hex2dec(0x12b7), -3, 3, 0, 0], [hex2dec(0x12b8), -2, 2, 0, 0],
+	[hex2dec(0x12b9), -1, 1, 0, 0], [hex2dec(0x12ba), 0, 0, 0, 0], [hex2dec(0x12bb), 1, -1, 0, 0],
+	[hex2dec(0x12bc), 2, -2, 0, 0], [hex2dec(0x12bd), 3, -3, 0, 0]
+]
+'''
+yewindex = [ [hex2dec(0x12ba), 0, 0, 0, 0] ]
+yewindex2 = [ [hex2dec(0x12b9), -1, 1, 0, 0] ]
 
 TREE = 0
 LEAVES = 1
@@ -253,27 +287,31 @@ def createyewtree( player, arguments, target):
 def addtree(socket, command, arguments):
 	if len(arguments) > 0:
 		item = str(arguments)
-		if item != 'yew' and item != 'random' and not jungleindex[item] and trees[item]:
-			if wolfpack.getdefinition(WPDT_ITEM, trees[item][TREE]) and wolfpack.getdefinition(WPDT_ITEM, trees[item][LEAVES]):
-				socket.sysmessage( "Where do you want to place the tree '%s', '%s' ?" % ( trees[item][TREE], trees[item][LEAVES]) )
-				socket.attachtarget( "commands.addtree.createtree", [ item ] )
-		elif item == 'yew':
-			socket.sysmessage( "Where do you want to place the yew tree?" )
-			socket.attachtarget( "custom.addtree.createyewtree", [] )
-			return
-		elif jungleindex[item]:
-			socket.sysmessage( "Where do you want to place the jungle tree [%s]?" %(item) )
-			socket.attachtarget( "commands.addtree.createjungletree", [item] )
-			return
-		elif item == 'random':
-			randomtrees = [ randint(0,2), randint(0,12), randint(2,12), randint(0,21), randint(13,15), randint(16,21) ]
+		if item == 'random':
+			randomtrees = [ randint(0,2), randint(0,12), randint(2,12), randint(0,20), randint(13,15), randint(16,20) ]
 			randomtree = randint( 0, 5 )
 			item = str( treeindex[ int( randomtrees[ randomtree ] ) ] )
 			if trees[item]:
 				if wolfpack.getdefinition(WPDT_ITEM, trees[item][TREE]) and wolfpack.getdefinition(WPDT_ITEM, trees[item][LEAVES]):
 					socket.sysmessage( "Where do you want to place the tree '%s', '%s' ?" % ( trees[item][TREE], trees[item][LEAVES]) )
-					socket.attachtarget( "commands.addtree.createtree", [ item ] )
+					socket.attachitemtarget( 'commands.addtree.createtree', trees[item][2], 0, 0, 0, [item])
 				return
+		elif item == 'yew':
+			socket.sysmessage( "Where do you want to place the yew tree?" )
+			socket.attachitemtarget( 'commands.addtree.createyewtree', yewindex, 0, 0, 0, [])
+			return
+		elif (item !='jungle1' and item !='jungle2' and item !='jungle3' and item !='jungle4' and item !='jungle5' and item !='jungle6' and item !='jungle7') and trees[item]:
+			if wolfpack.getdefinition(WPDT_ITEM, trees[item][TREE]) and wolfpack.getdefinition(WPDT_ITEM, trees[item][LEAVES]):
+				socket.sysmessage( "Where do you want to place the tree '%s', '%s' ?" % ( trees[item][TREE], trees[item][LEAVES]) )
+				socket.attachitemtarget( 'commands.addtree.createtree', trees[item][2], 0, 0, 0, [item])
+		elif jungleindex[item]:
+			socket.sysmessage( "Where do you want to place the jungle tree [%s]?" %(item) )
+			socket.attachitemtarget( 'commands.addtree.createjungletree', [jungleindex[item][1]], 0, 0, 0, [jungleindex[item][0]] )
+			return
+		else:
+			socket.sysmessage( "Usage: addtree [id, yew, jungle[1-7], random")
+	else:
+		socket.sysmessage( "Usage: addtree [id, yew, jungle[1-7], random")
 
 def onLoad():
 	wolfpack.registercommand('addtree', addtree)
