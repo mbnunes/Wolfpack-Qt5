13fa9b311cefed180fe379e27f5116e15356115d
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-04-12 10:19:47 +0000

    *** empty log message ***

--- server/release/scripts/wolfpack/time.py
+++ server/release/scripts/wolfpack/time.py
@@ -8,42 +8,42 @@ import _wolfpack.time
 
 """
 	\function wolfpack.time.minute
-	\return WPMinute
+	\return INT
 	\description Return the minute of the hour for the current game time. Range: 0 - 59
 """
 minute = _wolfpack.time.minute
 
 """
 	\function wolfpack.time.hour
-	\return WPHour
+	\return INT
 	\description Return the current hour of the day. Range: 0 - 23
 """
 hour = _wolfpack.time.hour
 
 """
 	\function wolfpack.time.days
-	\return Days Passed
+	\return INT
 	\description Returns the days elapsed since the initialization of the game time.
 """
 days = _wolfpack.time.days
 
 """
 	\function wolfpack.time.minutes
-	\return Minutes Passed
+	\return INT
 	\description Return the minutes elapsed since the initialization of the game time.
 """
 minutes = _wolfpack.time.minutes
 
 """
 	\function wolfpack.time.currenttime
-	\return CurrentTime
+	\return INT
 	\description Return the current servertime in miliseconds since start of the gameserver.
 """
 servertime = _wolfpack.currenttime
 
 """
 	\function wolfpack.time.currentdatetime
-	\return CurrentDateTime, STRING
+	\return STRING
 	\description Return the current real date and time as a string.
 """
 currentdatetime =  _wolfpack.currentdatetime
@@ -51,7 +51,7 @@ currentdatetime =  _wolfpack.currentdatetime
 
 """
 	\function wolfpack.time.currentlightlevel
-	\return LightLevel
+	\return INT
 	\description Return the current outside lightlevel.
 """
 currentlightlevel = _wolfpack.time.currentlightlevel
