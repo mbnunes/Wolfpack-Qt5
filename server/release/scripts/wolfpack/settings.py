8513890a97cfa469fee30ea6ebd3ff004f6b904e
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-04-12 00:45:50 +0000

    More documentation! :D

--- server/release/scripts/wolfpack/settings.py
+++ server/release/scripts/wolfpack/settings.py
@@ -15,26 +15,84 @@
 import _wolfpack.settings
 import _wolfpack
 
+"""
+	\function wolfpack.settings.getBool
+	\param section
+	\param key
+	\param default
+	\return Booling
+	\description Returns the booling value for the key in a given section.
+"""
 def getBool( section, key, default ):
 	return _wolfpack.settings.getBool()
 
+"""
+	\function wolfpack.settings.setBool
+	\param section
+	\param key
+	\param default
+	\return None
+	\description Sets a booling value for the key in a given section.
+"""
 def setBool( section, key, value ):
 	return _wolfpack.settings.setBool( section, key, value )
 
+"""
+	\function wolfpack.settings.getNumber
+	\param section
+	\param key
+	\param default
+	\return Number
+	\description Returns the number value for the key in a given section.
+"""
 def getNumber( section, key, default ):
 	return _wolfpack.settings.getNumber( section, key, default )
 
+"""
+	\function wolfpack.settings.setNumber
+	\param section
+	\param key
+	\param default
+	\return None
+	\description Sets a number value for the key in a given section.
+"""
 def setNumber( section, key, value ):
 	return _wolfpack.settings.setNumber( section, key, value )
 
+"""
+	\function wolfpack.settings.getString
+	\param section
+	\param key
+	\param default
+	\return String
+	\description Returns the string value for the key in a given section.
+"""
 def getString( section, key, default ):
 	return _wolfpack.settings.getString( section, key, default )
 
+"""
+	\function wolfpack.settings.setString
+	\param section
+	\param key
+	\param default
+	\return None
+	\description Sets a string value for the key in a given section.
+"""
 def setString( section, key, value ):
 	return _wolfpack.settings.setString( section, key, value )
 
+"""
+	\function wolfpack.settings.reload
+	\return None
+	\description Reload the settings.
+"""
 def reload():
 	return _wolfpack.settings.reload()
 
+"""
+	\function wolfpack.settings.save
+	\return None
+	\description Saves the settings.
+"""
 def save():
 	return _wolfpack.settings.save()
--- server/release/scripts/wolfpack/time.py
+++ server/release/scripts/wolfpack/time.py
@@ -6,23 +6,52 @@
 
 import _wolfpack.time
 
-# Return the minute of the hour for the current game time. Range: 0 - 59
+"""
+	\function wolfpack.time.minute
+	\return WPMinute
+	\description Return the minute of the hour for the current game time. Range: 0 - 59
+"""
 minute = _wolfpack.time.minute
 
-# Return the current hour of the day. Range: 0 - 23
+"""
+	\function wolfpack.time.hour
+	\return WPHour
+	\description Return the current hour of the day. Range: 0 - 23
+"""
 hour = _wolfpack.time.hour
 
-# Return the days elapsed since the initialization of the game time
+"""
+	\function wolfpack.time.days
+	\return Days Passed
+	\description Returns the days elapsed since the initialization of the game time.
+"""
 days = _wolfpack.time.days
 
-# Return the minutes elapsed since the initialization of the game time
+"""
+	\function wolfpack.time.minutes
+	\return Minutes Passed
+	\description Return the minutes elapsed since the initialization of the game time.
+"""
 minutes = _wolfpack.time.minutes
 
-# Return the current servertime in miliseconds since start of the gameserver.
+"""
+	\function wolfpack.time.currenttime
+	\return CurrentTime
+	\description Return the current servertime in miliseconds since start of the gameserver.
+"""
 servertime = _wolfpack.currenttime
 
-# Return the current real date and time as a string.
+"""
+	\function wolfpack.time.currentdatetime
+	\return CurrentDateTime, STRING
+	\description Return the current real date and time as a string.
+"""
 currentdatetime =  _wolfpack.currentdatetime
 
-# Return the current outside lightlevel.
+
+"""
+	\function wolfpack.time.currentlightlevel
+	\return LightLevel
+	\description Return the current outside lightlevel.
+"""
 currentlightlevel = _wolfpack.time.currentlightlevel
