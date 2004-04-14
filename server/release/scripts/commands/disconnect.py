ef0fec635fcf31d4d9e8eee36119419bcd1d83b4
naddel_tdv <naddel_tdv@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-04-14 00:21:58 +0000

    .disconnect command

---
+++ server/release/scripts/commands/disconnect.py
@@ -0,0 +1,22 @@
+import wolfpack
+
+"""
+	\command disconnect
+	\description Disconnect the client of the chosen char.
+	\usage - <code>disconnect</code>
+"""
+
+def disconnect( socket, command, argstring):
+	socket.sysmessage( "Which client do you want to disconnect?" )
+	socket.attachtarget( "commands.disconnect.disconnecttarget", [] )
+
+def disconnecttarget( char, args, target ):
+	if not target.char or target.char.npc:
+		char.socket.sysmessage( "That is not a valid character." )
+	if target.char:
+		target.char.socket.disconnect()
+		target.char.update()
+	return 1
+	
+def onLoad():
+	wolfpack.registercommand( "disconnect", disconnect )
