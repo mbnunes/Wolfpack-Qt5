1a7cc6a2a9bef9224fc82092ebda1dc040e7b8ca
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-05-16 21:35:36 +0000

    A nice debugging command (.eval) and a multibag for testing.

---
+++ server/release/scripts/testing/multibag.py
@@ -0,0 +1,31 @@
+
+import wolfpack
+from wolfpack import console
+from wolfpack.consts import *
+
+#
+# Add multiple instances of items based on tags
+# defined for the item on creation.
+#
+def onAttach(object):
+	if not object.isitem():
+		return
+
+	if not object.hastag('itemdef'):
+		console.log(LOG_ERROR, "Missing itemdef tag for multibag object.\n")
+		return
+		
+	if not object.hastag('itemamount'):
+		console.log(LOG_ERROR, "Missing itemamount tag for multibag object.\n")
+		return
+		
+	id = str(object.gettag('itemdef'))
+	amount = int(object.gettag('itemamount'))
+	
+	for i in range(0, amount):
+		item = wolfpack.additem(id)
+		item.container = object
+	
+	object.deltag('itemdef')
+	object.deltag('itemamount')
+	object.removeevent('testing.multibag')
