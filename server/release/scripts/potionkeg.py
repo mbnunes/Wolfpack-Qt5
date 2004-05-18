d337ca1065ca77e1892adee6890d769dd5ae9e88
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-05-18 08:28:12 +0000

    Trying to make onDropOnItem work for potion kegs... But for some reason it's not working at all. I follow the example thats in keyring.py and it still won't work with a damn... Bug maybe? :/

--- server/release/scripts/potionkeg.py
+++ server/release/scripts/potionkeg.py
@@ -15,76 +15,89 @@ POT_NAME = 3
 KEG_NAME = 4
 POT_DEF = 5
 
-def onUse(char, keg):
+### Filling the keg ###
+def onDropOnItem(potionkeg, potion):
+	if ('potionkeg' not in potionkeg.events) or ('potions' not in potion.events):
+		return 0
+
+	char = potion.container
 	socket = char.socket
-	kegfill = int(keg.gettag('kegfill'))
+
+	if not potion.hastag('potiontype') or not potionkeg.hastag('kegfill'):
+		socket.sysmessage("Only potions may be added to a potion keg!")
+		return 0
+
+	char.say("Test")
+	"""
+	char.say("Step 1")
+	if potionkeg.id in [ hex2dec(0x1ad6), hex2dec(0x1ad7) ]:
+		char.say("Step 2")
+		if potionkeg.hastag('kegfill') and potionkeg.hastag('potiontype'):
+			char.say("Step 3")
+			if potion.gettag('potiontype') == potionkeg.gettag('potiontype'):
+				kegfill = potionkeg.gettag('kegfill')
+				if kegfill < 100:
+					kegfill += 1
+					potionkeg.settag('kegfill', kegfill)
+					potion.delete()
+					return 1
+				else:
+					# The keg will not hold any more!
+					socket.clilocmessage(502233)
+					return 1
+			else:
+				# You decide that it would be a bad idea to mix different types of potions.
+				socket.clilocmessage(502236)
+				return 1
+		else:
+			potionkeg.settag( 'potiontype', potion.gettag('potiontype') )
+			potionkeg.settag( 'kegfill', 1 )
+			potionkeg.name = potions[ potion.gettag('potiontype') ][ KEG_NAME ]
+			consumePotion( char, potion, 1 )
+			return 1
+	else:
+		return 0
+	"""
+	return 1
+
+### End Filling ###
+
+def onUse(char, potionkeg):
+	socket = char.socket
+	kegfill = int(potionkeg.gettag('kegfill'))
 	backpack = char.getbackpack()
 
-	if keg.hastag('potiontype'):
-		checkkegstatus( char, keg, keg.gettag('potiontype') )
+	if potionkeg.hastag('potiontype'):
+		checkkegstatus( char, keg, potionkeg.gettag('potiontype') )
 
 	if kegfill > 0:
 		count = backpack.countitems(['f0e'])
 		if count >= 1:
 			backpack.removeitems(['f0e'], 1)
-			newpot = wolfpack.additem(potions[keg.gettag('potiontype')][POT_DEF])
+			newpot = wolfpack.additem(potions[potionkeg.gettag('potiontype')][POT_DEF])
 			newpot.decay = 1
 			newpot.magic = 1
 			if not tobackpack(newpot, char):
 				newpot.update()
 			kegfill -= 1
-			keg.settag('kegfill', kegfill )
+			potionkeg.settag('kegfill', kegfill )
 			if kegfill == 0:
-				if keg.name != "#1041641":
-					keg.name = '#1041641'
-				keg.deltag('potiontype')
+				if potionkeg.name != "#1041641":
+					potionkeg.name = '#1041641'
+				potionkeg.deltag('potiontype')
 		else:
 			socket.clilocmessage(500315)
 
 	else:
-		if keg.name != "#1041641":
-			keg.name = '#1041641'
-		if keg.hastag('potiontype'):
-			keg.deltag('potiontype')
+		if potionkeg.name != "#1041641":
+			potionkeg.name = '#1041641'
+		if potionkeg.hastag('potiontype'):
+			potionkeg.deltag('potiontype')
 
 	return 1
 
-def onDropOnItem(keg, potion):
-	char = potion.container
-
-	if 'potionkeg' not in keg.events or 'potion' not in potion.events:
-		char.say("Error with events!")
-		return 0
-
-	if not potion.hastag('potiontype'):
-		return 0
-
-	if keg.id == hex2dec(0x1ad6) or keg.id == hex2dec(0x1ad7):
-		if keg.hastag('kegfill') and keg.hastag('potiontype'):
-			if potion.gettag('potiontype') == keg.gettag('potiontype'):
-				kegfill = keg.gettag('kegfill')
-				if kegfill < 100:
-					kegfill += 1
-					keg.settag('kegfill', kegfill)
-					potion.delete()
-					return 1
-				else:
-					# 502233 The keg will not hold any more!
-					potion.say("Error!")
-					return 1
-			else:
-				# 502236 You decide that it would be a bad idea to mix different types of potions.
-				return 1
-		else:
-			keg.settag( 'potiontype', potion.gettag('potiontype') )
-			keg.settag( 'kegfill', 1 )
-			keg.name = potions[ potion.gettag('potiontype') ][ KEG_NAME ]
-			consumePotion( char, potion, 1 )
-			return 1
-	else:
-		return 0
-
-def checkkegstatus(char, keg, potiontype):
-	if keg.name != potions[ potiontype ][ KEG_NAME ]:
-		keg.name = potions[ potiontype ][ KEG_NAME ]
+def checkkegstatus(char, potionkeg, potiontype):
+	if potionkeg.name != potions[ potiontype ][ KEG_NAME ]:
+		potionkeg.name = potions[ potiontype ][ KEG_NAME ]
 	return 1
+
