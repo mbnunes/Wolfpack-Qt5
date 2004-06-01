243612c36b46d28f64414e8b8219925e85a82d58
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-06-01 21:32:02 +0000

    This should just set guild abbreviations to max of 3 chars. Also extends the baseid for items to 64, just to match npcs. Although... it would be rather insane to have baseids in that length, but just to be safe... :)

--- server/src/world.cpp
+++ server/src/world.cpp
@@ -92,9 +92,9 @@ struct {
 	{ "guilds", "CREATE TABLE guilds ( \
 	serial int(11) NOT NULL default '0', \
 	name varchar(255) NOT NULL default '', \
-	abbreviation varchar(255) NOT NULL default '', \
+	abbreviation char(3) NOT NULL default '', \
 	charta LONGTEXT NOT NULL, \
-	website varchar(255) NOT NULL default '', \
+	website varchar(255) NOT NULL default 'http://www.wpdev.org', \
 	alignment int(2) NOT NULL default '0', \
 	leader int(11) NOT NULL default '-1', \
 	founded int(11) NOT NULL default '0', \
@@ -206,7 +206,7 @@ struct {
 	sellprice int(11) NOT NULL default '0',\
 	buyprice int(11) NOT NULL default '0',\
 	restock smallint(5)  NOT NULL default '0',\
-	baseid varchar(32) NOT NULL default '',\
+	baseid varchar(64) NOT NULL default '',\
 	PRIMARY KEY (serial)\
 	);" },
 
