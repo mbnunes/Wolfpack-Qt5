f4faca783b9661c92a7922c700748fa7866a35ec
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-07-09 10:30:58 +0000

    I almost forgot to commit this

---
+++ server/release/scripts/commands/invis.py
@@ -0,0 +1,24 @@
+#===============================================================#
+#   )      (\_     | WOLFPACK 13.0.0 Scripts
+#  ((    _/{  "-;  | Created by: Dreoth
+#   )).-' {{ ;'`   | Revised by:
+#  ( (  ;._ \\ ctr | Last Modification: Created
+#===============================================================#
+"""
+	\command invis
+	\description Toggle invisibility.
+"""
+
+import wolfpack
+from wolfpack.utilities import booleantoggle
+
+def onLoad():
+	wolfpack.registercommand( "invis", commandInvis )
+	return
+
+def commandInvis( socket, cmd, args ):
+	char = socket.player
+	char.invisible = booleantoggle( char.invisible )
+	char.update()
+	socket.sysmessage( "Invisible is now '%i'." % char.invisible )
+	return True
