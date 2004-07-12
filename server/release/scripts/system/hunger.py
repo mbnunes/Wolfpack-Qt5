38ad5483867001d4efba3675ecf24c509db7a67b
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-07-12 16:06:27 +0000

    Fix

--- server/release/scripts/system/hunger.py
+++ server/release/scripts/system/hunger.py
@@ -11,6 +11,5 @@ def onTimeChange( player ):
 			player.hitpoints -= 1
 			player.update()
 			player.socket.sysmessage( "Your stomach hurts from the lack of food..." )
-		return True
-	else:
-		return False
+	
+	return False
