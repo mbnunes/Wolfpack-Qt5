83f9def29440adde41e5ecb962699990822f1ad9
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-08-12 00:56:03 +0000

    Useful fix by Incanus, healers should work now.

---
+++ server/release/scripts/contextmenus/healermenu.py
@@ -0,0 +1,27 @@
+#################################################################
+#   )      (\_     # WOLFPACK 13.0.0 Scripts                    #
+#  ((    _/{  "-;  # Created by: codex                          #
+#   )).-' {{ ;'`   # Revised by: Incanus                        #
+#  ( (  ;._ \\ ctr # Last Modification:                         #
+#################################################################
+
+def onContextCheckVisible(char, target, tag):
+	if( int( tag ) == 1 ):
+		return char.dead
+
+	return True
+
+def onContextEntry( char, healer, tag ):
+	if( int( tag ) == 1 ):
+		if not char or not healer:
+			return False
+
+		if healer.dead:
+			char.socket.clilocmessage( 501040  ) # The resurrecter must be alive
+			return False
+
+		char.socket.clilocmessage( 3002069  ) # Resurrection
+		char.soundeffect( 0x215 )
+		char.resurrect()
+
+	return True
--- server/release/scripts/npc/healer.py
+++ server/release/scripts/npc/healer.py
@@ -1,33 +1,3 @@
-#################################################################
-#   )      (\_     # WOLFPACK 13.0.0 Scripts                    #
-#  ((    _/{  "-;  # Created by: codex                          #
-#   )).-' {{ ;'`   # Revised by:                                #
-#  ( (  ;._ \\ ctr # Last Modification: Created                 #
-#################################################################
 
-import wolfpack
-from wolfpack.consts import *
-
-def onContextEntry( char, healer, tag ):
-	if( int( tag ) == 1 ):
-		healer.say("Hold still!")
-		if not char or not healer:
-			return False
-
-		if healer.dead:
-			char.socket.clilocmessage( 501040, "", YELLOW, NORMAL ) # The resurrecter must be alive
-			return False
-
-		if char.dead:
-			char.socket.clilocmessage( 3002069, "", YELLOW, NORMAL ) # Resurrection
-			char.soundeffect( 0x215 )
-			char.resurrect()
-			return True
-		else:
-			char.socket.clilocmessage( 1060197, "", YELLOW, NORMAL ) # You are not dead, and thus cannot be resurrected!
-			return False
-
-		return True
-	else:
-		char.say("A HEALER! RESURRECT ME!")
-		return True
+# For Later Use
+# Context Menu Hooks are in contextmenus.healermenu
