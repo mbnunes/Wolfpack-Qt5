b3c056aef4c80f5b69f8c7cd599babb6e1d299f2
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-08-19 12:08:25 +0000

    Did this to make sure it forces Python2.3, some systems still use 2.2 for some reason.

--- server/build/configure.py
+++ server/build/configure.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.3
 #################################################################
 #   )      (\_     # Wolfpack 13.0.0 Build Script               #
 #  ((    _/{  "-;  # Created by: Wolfpack Development Team      #
