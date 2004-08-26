d5779a9230a4265141950e9b1d10d75b3e3a9c8d
kell_pt <kell_pt@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-08-26 00:52:13 +0000

    Create-once for python objects: target.char, target.item, target.pos.

--- server/src/python/target.h
+++ server/src/python/target.h
@@ -42,7 +42,11 @@ typedef struct
 	Coord_cl pos;
 	Q_UINT16 model;
 	SERIAL object;
+
+	PyObject *	py_pos;
+	PyObject *	py_obj;	// char or item
 } wpTarget;
+
 
 static PyObject* wpTarget_getAttr( wpTarget* self, char* name )
 {
@@ -51,30 +55,61 @@ static PyObject* wpTarget_getAttr( wpTarget* self, char* name )
 		Remember that this value should not be trusted.
 	*/
 	if ( !strcmp( name, "pos" ) )
-		return PyGetCoordObject( self->pos );
+	{
+		if ( !self->py_pos )
+			self->py_pos = PyGetCoordObject( self->pos );
+
+		Py_INCREF( self->py_pos );
+		return self->py_pos;
+	}
 	/*
 		\rproperty target.model If a static tile has been targetted by the client, this property contains the art id of the targetted static tile. If a character has been targetted,
 		this property contains the body id of the targetted character. Please note that this value is sent by the client and not determined by the server.
 	*/
 	else if ( !strcmp( name, "model" ) )
+	{
 		return PyInt_FromLong( self->model );
+	}
 	/*
 		\rproperty target.item If a valid item has been targetted, this property contains an <object id="item">item</object> object for the targetted item.
 	*/
 	else if ( !strcmp( name, "item" ) )
 	{
 		if ( isItemSerial( self->object ) )
-			return PyGetItemObject( FindItemBySerial( self->object ) );
+		{
+			if ( !self->py_obj )
+				self->py_obj = PyGetItemObject( FindItemBySerial( self->object ) );
+
+			Py_INCREF( self->py_obj );
+			return self->py_obj;
+		}
 	}
 	/*
 		\rproperty target.char If a valid character has been targetted, this property contains a <object id="char">char</object> object for the targetted character.
 	*/
 	else if ( !strcmp( name, "char" ) )
+	{
 		if ( isCharSerial( self->object ) )
-			return PyGetCharObject( FindCharBySerial( self->object ) );
+		{
+			if ( !self->py_obj )
+				self->py_obj = PyGetCharObject( FindCharBySerial( self->object ) );
 
+			Py_INCREF( self->py_obj );
+			return self->py_obj;
+		}
+	}
 	Py_RETURN_FALSE;
 }
+
+
+static void wpTarget_Dealloc( wpTarget * self )
+{
+	Py_XDECREF( self->py_pos	);
+	Py_XDECREF( self->py_obj	);
+
+	wpDealloc( (PyObject*) self );
+}
+
 
 static PyTypeObject wpTargetType =
 {
@@ -83,7 +118,7 @@ PyObject_HEAD_INIT( NULL )
 "wpTarget",
 sizeof( wpTargetType ),
 0,
-wpDealloc,
+( destructor )  wpTarget_Dealloc,
 0,
 ( getattrfunc ) wpTarget_getAttr,
 0,
@@ -93,6 +128,7 @@ wpDealloc,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 
 };
+
 
 static PyObject* PyGetTarget( cUORxTarget* target, Q_UINT8 map )
 {
@@ -109,9 +145,12 @@ static PyObject* PyGetTarget( cUORxTarget* target, Q_UINT8 map )
 	pos.z = target->z();
 	pos.map = map;
 
-	returnVal->pos = pos;
-	returnVal->object = target->serial();
-	returnVal->model = target->model();
+	returnVal->pos			= pos;
+	returnVal->object		= target->serial();
+	returnVal->model		= target->model();
+
+	returnVal->py_pos		= NULL;
+	returnVal->py_obj		= NULL;
 
 	return ( PyObject * ) returnVal;
 }
