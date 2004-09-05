f71817c4f4c0f0842d3d1c5b9ab2806f5f4ff182
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-09-05 22:27:05 +0000

    Fixes.

--- server/release/scripts/system/hunger.py
+++ server/release/scripts/system/hunger.py
@@ -4,12 +4,15 @@ import random
 import wolfpack.time
 
 def onTimeChange( player ):
-	if wolfpack.time.hour() in [ 0, 3, 6, 9, 12, 15, 18, 21 ]:
+	hungerrate = int(wolfpack.settings.getstring("Game Speed", "Hunger Rate", "2"))
+	hungerdamage = int(wolfpack.settings.getstring("General", "Hunger Damage", "0"))
+
+	if wolfpack.time.hour() in range( hungerrate, 23, hungerrate ):
 		if player.socket:
 			if player.hunger >= 1 and player.hunger <= 6:
 				player.hunger -= 1
 				player.socket.sysmessage( "Your stomach growls..." )
 			elif player.hunger == 0:
-				player.damage( 3, random.randint( 0, 3 ) )
+				player.damage( 3, random.randint( 0, hungerdamage ) )
 				player.socket.sysmessage( "Your stomach hurts from the lack of food..." )
 	return False
