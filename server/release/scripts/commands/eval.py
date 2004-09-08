df687bdc0234134904cc056039a0e7ed1090512b
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-09-08 03:16:29 +0000

    Nice little updated to .eval

--- server/release/scripts/commands/eval.py
+++ server/release/scripts/commands/eval.py
@@ -3,17 +3,23 @@
 # Keep that in mind
 
 import wolfpack
+from wolfpack.consts import *
+from wolfpack import *
 
 def evalcommand(socket, command, arguments):
-	script = "import wolfpack\nfrom wolfpack.consts import *\n%s\n\n" % arguments
+	#script = "import wolfpack\nfrom wolfpack.consts import *\n%s\n\n" % arguments
+	script = arguments
+	result = None
 	try:
-		code = compile(script, '<SOCKET>', 'exec')
-		eval(code, globals(), locals())
+		#code = compile(script, '<SOCKET>', 'exec')
+		#result = eval(script, globals(), locals())
+		result = eval(script)
 	except Exception, e:
 		socket.sysmessage('Python Error: ' + str(e))
 		return
 
 	socket.sysmessage('Executed the python scriptlet.')
+	socket.sysmessage('Result: ' + str(result))
 
 def onLoad():
 	wolfpack.registercommand('eval', evalcommand)
