0a79c11783613d1af1c068f8722c864354c785b2
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-09-09 03:31:15 +0000

    ok.. I think I'll leave it at this tonight... my brain is scrambled.

--- server/release/scripts/commands/kickban.py
+++ server/release/scripts/commands/kickban.py
@@ -2,12 +2,10 @@
 	\command kick
 	\description Kicks a connected client by target or account name.
 """
-
 """
 	\command ban
 	\description Kick-bans an account by target or account name.
 """
-
 """
 	\command unban
 	\description Unbans an account with the given account name.
@@ -15,7 +13,7 @@
 """
 
 import wolfpack
-from wolfpack.consts import LOG_MESSAGE
+import wolfpack.accounts
 
 def onLoad():
 	wolfpack.registercommand( "kick", commandKick )
@@ -25,24 +23,22 @@ def onLoad():
 def commandKick( socket, cmd, args ):
 	if len( args ) > 0:
 		accname = args.lower()
-		account = None
 		account = wolfpack.accounts.find( accname )
 		accountAction( socket, account, 'kick' )
 		return
 	else:
-		socket.sysmessage( "Please select the object for removal." )
+		socket.sysmessage( "Please select the character to kick." )
 		socket.attachtarget( "commands.remove.doKick", [] )
 	return True
 
 def commandBan( socket, cmd, args ):
 	if len( args ) > 0:
 		accname = args.lower()
-		account = None
 		account = wolfpack.accounts.find( accname )
 		accountAction( socket, account, 'ban' )
 		return
 	else:
-		socket.sysmessage( "Please select the object for removal." )
+		socket.sysmessage( "Please select the character to kick-ban." )
 		socket.attachtarget( "commands.remove.doBan", [] )
 	return False
 
@@ -62,17 +58,15 @@ def commandUnban( socket, cmd, args ):
 	myrank = playeraccount.rank
 	if len( args ) > 0:
 		accname = args.lower()
-		account = None
 		account = wolfpack.accounts.find( accname )
-		if account:
-			if account.rank > 1:
-				if playeraccount.authorized( 'Misc', 'May Block Staff Accounts' ):
-					continue
-				else:
-					socket.sysmessage( "You're not authorized to unblock staff members." )
-					return False
+		if account and account.rank <= myrank:
+			if account.rank > 1 and playeraccount.authorized( 'Misc', 'May Block Staff Accounts' ):
+				continue
+			else:
+				socket.sysmessage( "You're not authorized to unban staff members." )
+				return False
 			account.unblock()
-			socket.sysmessage( "You've unblocked the account %s." % account.name )
+			socket.sysmessage( "You've unban the account %s." % account.name )
 			return True
 	else:
 		socket.sysmessage( "You need to specify an account name to unban." )
@@ -85,22 +79,17 @@ def accountAction( socket, account, action ):
 	if account == None:
 		socket.sysmessage( "Failed to find an account with the given name." )
 		return False
-	elif account and myrank > account.rank:
-		if account.rank == 100:
-			socket.sysmessage( "You can not kick or ban admins!" )
+	elif account and account.rank <= myrank:
+		if account.rank > 1 and playeraccount.authorized( 'Misc', 'May Block Staff Accounts' ):
+			continue
+		else:
+			socket.sysmessage( "You're not authorized to kick or ban staff members." )
 			return False
-		if account.rank > 1:
-			if playeraccount.authorized( 'Misc', 'May Block Staff Accounts' ):
-				continue
-			else:
-				socket.sysmessage( "You're not authorized to kick or ban staff members." )
-				return False
 
 		if action == 'kick':
-			account.block()
 			for char in account.characters:
 				if char.socket:
-					char.soscket.sysmessage( "You've been kicked from the shard by the staff." )
+					char.socket.sysmessage( "You've been kicked from the shard by the staff." )
 					char.socket.disconnect()
 					socket.sysmessage( "You have disconnected %s." % char.name )
 			return True
@@ -108,8 +97,8 @@ def accountAction( socket, account, action ):
 			account.block()
 			for char in account.characters:
 				if char.socket:
-					char.soscket.sysmessage( "You've been kick-banned from the shard by the staff." )
+					char.socket.sysmessage( "You've been kick-banned from the shard by the staff." )
 					char.socket.disconnect()
 					socket.sysmessage( "You have kick-banned %s." % char.name )
 			return True
-		return False
+	return False
