8f6c0216eea7c726372a27e12082964cd1df5008
dark-storm <dark-storm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-10-05 13:22:03 +0000

    go boom now uses less boom of server cpu ^^

--- server/release/scripts/commands/boom.py
+++ server/release/scripts/commands/boom.py
@@ -12,25 +12,20 @@ def boom_timer(object, args):
 		
 	if 0.5 >= random.random():
 		t = random.randint(0, 2)
-
-		item = wolfpack.additem('eed')
-		item.moveto(x, y, z, map)
 		
 		# Fire Column
 		if t == 0:
-			wolfpack.effect(0x3709, pos, 10, 30)
-			item.soundeffect(0x208)
+			pos.effect(0x3709, 10, 30)
+			pos.soundeffect(0x208)
 		
 		# Explosion
 		elif t == 1:
-			wolfpack.effect(0x36bd, pos, 20, 10)
-			item.soundeffect(0x307)
+			pos.effect(0x36bd, 20, 10)
+			pos.soundeffect(0x307)
 		
 		# Ball of fire
 		elif t == 2:
-			wolfpack.effect(0x36fe, pos, 10, 10)
-			
-		item.delete()
+			pos.effect(0x36fe, 10, 10)
 
 # Go boom
 def boom(socket, command, arguments):
