54c22fcd379194100bb90dea443900fff14958ea
incanus <incanus@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2004-11-22 18:01:17 +0000

    Added tilecolorz command.

--- server/release/scripts/commands/tilecolor.py
+++ server/release/scripts/commands/tilecolor.py
@@ -8,6 +8,17 @@
 	from the given list for every tile that is found.		
 """
 
+"""
+	\command tilecolorz
+	\description Same as tilecolor, but limited to one z coordinate.
+	\usage - <code>tilecolorz z color</code>
+	- <code>tilecolor z color1,color2,...,colorn</coode>
+	Z is the height where the items should be colored.
+	Color is the id of the color you want to hue the items in.
+	If you give more than one color, one color is randomly selected
+	from the given list for every tile that is found.		
+"""
+
 import wolfpack
 from wolfpack.consts import *
 import random
@@ -58,6 +69,52 @@ def tileResponse(player, arguments, target):
 				item.update()
 
 #
+# Gather another corner if neccesary and start tiling
+#
+def tileResponsez(player, arguments, target):
+	if len(arguments) < 3:
+		player.socket.sysmessage('Please select the second corner.')
+		player.socket.attachtarget("commands.tilecolor.tileResponsez", list(arguments) + [target.pos])
+		return
+
+	x1 = min(arguments[2].x, target.pos.x)
+	x2 = max(arguments[2].x, target.pos.x)
+	y1 = min(arguments[2].y, target.pos.y)
+	y2 = max(arguments[2].y, target.pos.y)
+	z = int( arguments[0] )
+	colors = arguments[1]
+	
+	unlimited = player.account.authorized('Misc', 'Unlimited Tilecolor')
+	count = ((x2 - x1) + 1) * ((y2 - y1) + 1)
+	
+	# Cap at 500 items if not an admin is using it
+	if not unlimited and count > 250:
+		player.socket.sysmessage('You are not allowed to tile more than 250 items at once.')
+		return
+
+	player.log(LOG_MESSAGE, "Dying items (%s) from %u,%u,%u to %u,%u,%u.\n" % (", ".join(colors), x1, y1, z, x2, y2, z))
+	player.socket.sysmessage('Dying items from %u,%u,%u to %u,%u,%u.' % (x1, y1, z, x2, y2, z))
+	
+	try:
+		colors = map(hex2dec, colors)
+	except:
+		player.socket.sysmessage('You have to pass a comma separated list of numbers.')
+		player.socket.sysmessage('Hexadecimal numbers have to be in the 0x123 format.')
+		raise
+	
+	mapid = player.pos.map
+	for x in range(x1, x2 + 1):
+		for y in range(y1, y2 + 1):
+			items = wolfpack.items(x, y, mapid, 0)
+			for item in items:
+				if( item.pos.z == z ):
+					color = random.choice(colors)
+					# This should be a trace log
+					#player.log(LOG_MESSAGE, "Dying item 0x%x (%s) from color 0x%x to 0x%x.\n" % (item.serial, item.baseid, item.color, color))
+					item.color = color
+					item.update()
+
+#
 # Gather arguments and validate id list.
 #
 def commandTilecolor(socket, command, arguments):
@@ -72,8 +129,21 @@ def commandTilecolor(socket, command, arguments):
 	socket.sysmessage('Please select the first corner.')
 	socket.attachtarget('commands.tilecolor.tileResponse', [ids])
 
+def commandTilecolorz(socket, command, arguments):
+	if arguments.count(' ') != 1:
+		socket.sysmessage('Usage: .tilecolorz z id{,id}')
+		return
+
+	(z, ids) = arguments.split(' ')
+
+	ids = ids.split(',')
+	
+	socket.sysmessage('Please select the first corner.')
+	socket.attachtarget('commands.tilecolor.tileResponsez', [z, ids])
+
 #
 # Register the command
 #
 def onLoad():
 	wolfpack.registercommand("tilecolor", commandTilecolor)
+	wolfpack.registercommand("tilecolorz", commandTilecolorz)
