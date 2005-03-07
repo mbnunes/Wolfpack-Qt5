21f401d4a15af7990239def65878261a1fbd6eb5
dreoth <dreoth@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2005-03-07 02:40:30 +0000

    Allowed removing items through serials. This is useful for hidden items like resource gems.

--- server/release/scripts/commands/remove.py
+++ server/release/scripts/commands/remove.py
@@ -5,16 +5,33 @@
 
 import wolfpack
 from wolfpack.consts import LOG_MESSAGE
+from wolfpack.utilities import hex2dec
 
 def onLoad():
 	wolfpack.registercommand( "remove", commandRemove )
 
 def commandRemove(socket, cmd, args):
+	if len(args) > 0:
+		serial = hex2dec(args)
+		doRemoveSerial( socket, serial )
+		return True
 	socket.sysmessage( "Please select the object for removal." )
 	if( socket.account.authorized('Misc', 'May Remove Players') ):
 		socket.sysmessage( "Caution: This can remove players!" )
 	socket.attachtarget( "commands.remove.doRemove", [] )
 	return True
+
+def doRemoveSerial( socket, serial ):
+	object = wolfpack.findobject( serial )
+	if object and object.isitem():
+		socket.sysmessage("Found item object 0x%x for deletion." % serial )
+		socket.player.log( LOG_MESSAGE, "Removed item 0x%x.\n" % serial )
+		object.delete()
+		return True
+	else:
+		socket.sysmessage("Object 0x%x could not be found for deletion." % serial )
+		return False
+	return False
 
 def doRemove( char, args, target ):
 	if target.item:
