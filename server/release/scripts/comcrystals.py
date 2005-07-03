abbdcc88b1a87d2f9f43b694045ea62dd6f571bb
naddel <naddel@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2005-07-03 14:06:48 +0000

    update on comcrystals (receiver/sender); linking and recharching works now, the only thing missing is transmitting messages (i think we need a new event for that...)

--- server/release/scripts/comcrystals.py
+++ server/release/scripts/comcrystals.py
@@ -42,14 +42,57 @@ def receivers( item ):
 			amount += 1
 	return amount
 
+def activate_sender( item ):
+	item.settag( "active", 0 )
+	item.id = 0x1ecd
+	item.update()
+
+def activate_receiver( item ):
+	item.settag( "active", 0 )
+	item.id = 0x1ed1
+	item.update()
+
+def deactivate( item ):
+	item.deltag( "active" )
+	item.id = 0x1ed0
+	item.update()
+
+def deltags_receiver( item ):
+	try:
+		for i in item.tags:
+			if i.startswith( "receiver_" ):
+				item.deltag(i)
+				receiver = wolfpack.finditem( int(i.split("_")[1]) )
+				receiver.deltag( "sender" )
+	except:
+		pass
+	return
+
+def deltags_sender( item ):
+	if item.hastag( "sender" ):
+		sender = wolfpack.finditem( int(item.gettag("sender")) )
+		sender.deltag( "receiver_%s" % item.serial )
+		sender.resendtooltip()
+	return
+
+def onDelete( item ):
+	if item.baseid == "comcrystal_sender":
+		deltags_receiver( item )
+	else:
+		deltags_sender( item )
+	return
+
 def onUse( char, item ):
 	if not char.canreach( item, 2 ):
 		char.socket.clilocmessage( 1019045 ) # I can't reach that.
 		return False
-	char.socket.attachtarget( "comcrystals.response", [item.serial] )
+	if item.baseid == "comcrystal_sender":
+		char.socket.attachtarget( "comcrystals.response_sender", [item.serial] )
+	else:
+		char.socket.attachtarget( "comcrystals.response_receiver", [item.serial] )
 	return True
 
-def response( char, args, target ):
+def response_sender( char, args, target ):
 	item = wolfpack.finditem( args[0] )
 	if not item:
 		return False
@@ -59,17 +102,17 @@ def response( char, args, target ):
 
 	if target.item and target.item.serial == item.serial:
 		if active(item):
-			item.deltag( "active" )
+			deactivate( item )
 			char.socket.clilocmessage( 500672 ) # You turn the crystal off.
 		else:
 			if int(charges(item)) > 0:
-				item.settag( "active", 0 )
+				activate_sender( item )
 				char.socket.clilocmessage( 500673 ) # You turn the crystal on.
 			else:
 				char.socket.clilocmessage( 500676 ) # This crystal is out of charges.
-		item.resendtooltip()
+		#item.resendtooltip()
 
-	elif target.item and target.item.baseid == "receiver_crystal":
+	elif target.item and target.item.baseid == "comcrystal_receiver":
 		if receivers(item) >= 10:
 			char.socket.cliloc( 1010042 ) # This broadcast crystal is already linked to 10 receivers.
 			return False
@@ -110,16 +153,49 @@ def response( char, args, target ):
 		char.socket.clilocmessage( 500681 ) # You cannot use this crystal on that.
 	return True
 
+def response_receiver( char, args, target ):
+	item = wolfpack.finditem( args[0] )
+	if not item:
+		return False
+	if not item.getoutmostchar() == char and not char.canreach( target.pos, 2 ):
+		char.socket.clilocmessage( 1019045 ) # I can't reach that.
+		return False
+
+	if target.item and target.item.serial == item.serial:
+		if active(item):
+			deactivate( item )
+			char.socket.clilocmessage( 500672 ) # You turn the crystal off.
+		else:
+			activate_receiver( item )
+			char.socket.clilocmessage( 500673 ) # You turn the crystal on.
+
+	elif target.char:
+		if item.hastag( "sender" ):
+			deltags_sender( item )
+			item.deltag( "sender" )
+			char.socket.clilocmessage( 1010044 ) # You unlink the receiver crystal.
+		else:
+			char.socket.clilocmessage( 1010045 ) # That receiver crystal is not linked.
+		#item.resendtooltip()
+
+	else:
+		if getname(target.item.baseid) in recharges.keys():
+			char.socket.clilocmessage( 500677 ) # This crystal cannot be recharged.
+			return True
+		char.socket.clilocmessage( 1010045 ) # That receiver crystal is not linked.
+	return True
 
 def onShowTooltip(player, object, tooltip):
-	tooltip.reset()
-	tooltip.add(0xF9060 + object.id, '')
 	if active(object):
 		tooltip.add(1060742,"")
 	else:
 		tooltip.add(1060743,"")
-	tooltip.add( 1060745,"" )
-	tooltip.add( 1060741, str(charges(object)) )
+	if object.baseid == "comcrystal_sender":
+		tooltip.add( 1060745,"" )
+	else:
+		tooltip.add( 1060744,"" )
+	if object.baseid == "comcrystal_sender":
+		tooltip.add( 1060741, str(charges(object)) )
 	if receivers(object) > 0:
 		tooltip.add(1060746, str(receivers(object)) )
 	return
