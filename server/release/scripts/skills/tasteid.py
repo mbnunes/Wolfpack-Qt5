b4b0fb530f5bf2dcdbb7f4584c78d3a58789ace3
naddel <naddel@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2005-07-12 13:45:18 +0000

    tasteid

--- server/release/scripts/skills/tasteid.py
+++ server/release/scripts/skills/tasteid.py
@@ -19,13 +19,13 @@ def tasteid(char, skill):
 	if socket.hastag('skill_delay'):
 		if wolfpack.time.currenttime() < socket.gettag('skill_delay'):
 			socket.clilocmessage(500118)
-			return 1
+			return True
 		else:
 			socket.deltag('skill_delay')
 
-	socket.clilocmessage(502807)
+	socket.clilocmessage(502807) # What would you like to taste?
 	socket.attachtarget("skills.tasteid.response")
-	return 1
+	return True
 
 def response(char, args, target):
 	socket = char.socket
@@ -39,23 +39,43 @@ def response(char, args, target):
 
 	if target.item:
 		if not char.canreach(target.item, 2):
-			socket.clilocmessage(500312)
+			socket.clilocmessage(502815) # You are too far away to taste that.
 			return
 
-		if not target.item.hasscript( 'food' ):
-			socket.clilocmessage(502820)
-		else:
-			if not char.checkskill(TASTEID, 0, 1000):
-				socket.clilocmessage(502823)
+		if target.item.hasscript( 'food' ):
+			if char.checkskill(TASTEID, 0, 1000):
+				if target.item.hastag( 'poisoning_char' ):
+					target.item.say(1038284) # It appears to have poison smeared on it.
+				else:
+					target.item.say(1010600) # You detect nothing unusual about this substance.
 			else:
-				# TODO: Check for poison in the food.
-				socket.clilocmessage(502823)
+				target.item.say(502823) # You cannot discern anything about this substance.
+
+		elif target.item.hasscript( 'potions' ):
+			target.item.say(502813) # You already know what kind of potion that is.
+			name = target.item.name.split( '#' )
+			target.item.say( int(name[1]) )
+
+		elif target.item.hasscript( 'potionkeg' ):
+			kegfill = 0
+			if target.item.hastag( 'kegfill' ):
+				kegfill = target.item.gettag( 'kegfill' )
+			if kegfill <= 0:
+				target.item.say(502228) # There is nothing in the keg to taste!
+			else:
+				target.item.say(502229) # You are already familiar with this keg's contents.
+				name = target.item.name.split( '#' )
+				target.item.say( int(name[1]) )
+		else:
+			target.item.say( 502820 ) # That's not something you can taste.
 
 	elif target.char:
-		socket.clilocmessage(502816)
+		socket.clilocmessage(502816) # You feel that such an action would be inappropriate.
 
 	else:
-		socket.clilocmessage(502820)
+		socket.clilocmessage(502820) # That's not something you can taste.
+
+	return True
 
 def onLoad():
 	skills.register(TASTEID, tasteid)
