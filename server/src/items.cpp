aff74494a0cfa8f28c02764744d901910a9d19a6
darkstorm <darkstorm@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2005-08-13 13:31:14 +0000

    Fix so that .set layer no longer is able to crash the server (during save).

--- server/src/items.cpp
+++ server/src/items.cpp
@@ -1692,8 +1692,14 @@ stError* cItem::setProperty( const QString& name, const cVariant& value )
 	/*
 		\property item.layer The layer of the object, used with equipable objects.
 	*/
-	else
-		SET_INT_PROPERTY( "layer", layer_ )
+	else if (name == "layer") {
+		if (container_ && container_->isChar()) {
+			PROPERTY_ERROR(-3, "You may not change the layer of items that are currently equipped.");
+		} else {
+			SET_INT_PROPERTY("layer", layer_);
+		}
+	}
+		
 		/*
 		\property item.health The current health or durability of the object.
 		*/
