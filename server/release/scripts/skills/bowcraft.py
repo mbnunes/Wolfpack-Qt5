8ad0ef32d6dde7fe6bb6cba3c51d0d88d7965d09
incanus <incanus@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2005-09-12 23:16:18 +0000

    optional diffrent woods for bowcraft

--- server/release/scripts/skills/bowcraft.py
+++ server/release/scripts/skills/bowcraft.py
@@ -1,18 +1,38 @@
 #################################################################
 #   )      (\_     # WOLFPACK 13.0.0 Scripts                    #
 #  ((    _/{  "-;  # Created by: khpae                          #
-#   )).-' {{ ;'`   # Revised by:                                #
+#   )).-' {{ ;'`   # Revised by: Incanus                        #
 #  ( (  ;._ \\ ctr # Last Modification: Created                 #
 #################################################################
 
 from wolfpack import console
 from wolfpack.consts import *
-import math
-import wolfpack
+from wolfpack import properties, settings, getdefinition
+from math import ceil
 from system.makemenus import CraftItemAction, MakeMenu, findmenu, generateNamefromDef
-from wolfpack.utilities import hex2dec, tobackpack
+from wolfpack.utilities import hex2dec, tobackpack, tr
 from wolfpack.properties import itemcheck, fromitem
-import random
+
+extended_carpentry = int( settings.getbool("General", "Extended Carpentry", False, True) )
+
+if extended_carpentry:
+	WOOD = [
+			[tr('Beech Wood'), BOWCRAFT, 0, ['1bd7', '1bda', '1bdd', '1bde', '1bdf', '1be0', '1be1', '1be2'], 0, 'beech'],
+			[tr('Apple Wood'), BOWCRAFT, 300, ['board_apple', 'log_apple'], 1748, 'apple'],
+			[tr('Peach Wood'), BOWCRAFT, 350, ['board_peach', 'log_peach'], 1856, 'peach'],
+			[tr('Pear Wood'), BOWCRAFT, 400, ['board_pear', 'log_pear'], 1002, 'pear'],
+			[tr('Cedar Wood'), BOWCRAFT, 450, ['board_cedar', 'log_cedar'], 2107, 'cedar'],
+			[tr('Willow Wood'), BOWCRAFT, 500, ['board_willow', 'log_willow'], 1513, 'willow'],
+			[tr('Cypress Wood'), BOWCRAFT, 550, ['board_cypress', 'log_cypress'], 2413, 'cypress'],
+			[tr('Oak Wood'), BOWCRAFT, 600, ['board_oak', 'log_oak'], 1049, 'oak'],
+			[tr('Walnut Wood'), BOWCRAFT, 650, ['board_walnut', 'log_walnut'], 1839, 'walnut'],
+			[tr('Yew Wood'), BOWCRAFT, 700, ['board_yew', 'log_yew'], 1502, 'yew'],
+			[tr('Tropical Wood'), BOWCRAFT, 750, ['board_tropical', 'log_tropical'], 1889, 'tropical']
+	]
+else:
+	WOOD = [
+			[tr('Beech Wood'), BOWCRAFT, 0, ['1bd7', '1bda', '1bdd', '1bde', '1bdf', '1be0', '1be1', '1be2'], 0, 'beech'],
+	]
 
 #
 # Display fletching craftmenu.
@@ -29,8 +49,8 @@ def onUse(player, item):
 class FletchItemAction(CraftItemAction):
 	def __init__(self, parent, title, itemid, definition):
 		CraftItemAction.__init__(self, parent, title, itemid, definition)
-		self.markable = 1
-		self.stackable = 0
+		self.markable = True
+		self.stackable = False
 
 	#
 	# Check if we did an exceptional job.
@@ -48,7 +68,6 @@ class FletchItemAction(CraftItemAction):
 		minskill += penalty
 		maxskill += penalty
 		chance = ( player.skill[BOWCRAFT] - (minskill *0.65) ) / 10
-		#chance = ( (player.skill[BOWCRAFT] - minskill) / (maxskill - minskill) ) / 10.0
 
 		if chance > 100:
 			chance = 100
@@ -60,9 +79,7 @@ class FletchItemAction(CraftItemAction):
 		return chance
 
 	#
-	# For bowcraft we simply consume the maximum material available.
-	# Here we try to consume extra material to add to the amount of our
-	# items.
+	# Apply resname and color to the item.
 	#
 	def applyproperties(self, player, arguments, item, exceptional):
 		# Use all available resources if the item we make is
@@ -70,41 +87,65 @@ class FletchItemAction(CraftItemAction):
 		if self.stackable:
 			backpack = player.getbackpack()
 			count = -1
+			# count  how many items we could make
 			for (materials, amount, name) in self.materials:
 				items = backpack.countitems(materials)
 				if count == -1:
 					count = items / amount
 				else:
 					count = min(count, items / amount)
+
+			# count  how many items we could make
+			if( self.submaterial1 > 0 ):
+				material = self.parent.getsubmaterial1used(player, arguments)
+				material = self.parent.submaterials1[material]
+				items = backpack.countitems(material[3])
+				if count == -1:
+					count = items / self.submaterial1
+				else:
+					count = min(count, items / self.submaterial1)
+
+			# consume material
 			for (materials, amount, name) in self.materials:
-				backpack.removeitems( materials, count )
+				backpack.removeitems( materials, int(count * amount) )
+
+			# consume material
+			if self.submaterial1 > 0:
+				material = self.parent.getsubmaterial1used(player, arguments)
+				material = self.parent.submaterials1[material]
+				backpack.removeitems( material[3], int(count * self.submaterial1) )
+
 			if count != -1:
 				item.amount += count
 			else:
 				item.amount = 1 + count
 			item.update()
 
+			# Resend weight
+			player.socket.resendstatus()
 
-		# Distribute another 6 points randomly between the resistances an armor alread
-		# has. There are no tailored weapons.
-		if exceptional:
-			if itemcheck(item, ITEM_ARMOR) or itemcheck(item, ITEM_SHIELD):
-				# Copy all the values to tags
-				boni = [0, 0, 0, 0, 0]
+		else:
+			if extended_carpentry:
+				if self.submaterial1 > 0:
+					material = self.parent.getsubmaterial1used(player, arguments)
+					material = self.parent.submaterials1[material]
+					item.color = material[4]
+					item.settag('resname', material[5])
 
-				for i in range(0, 6):
-					boni[random.randint(0,4)] += 1
+			# Apply one-time boni
+			healthbonus = properties.fromitem(item, DURABILITYBONUS)
+			if healthbonus != 0:
+				bonus = int( ceil(item.maxhealth * (healthbonus / 100.0)) )
+				item.maxhealth = max(1, item.maxhealth + bonus)
+				item.health = item.maxhealth
 
-				item.settag('res_physical', fromitem(item, RESISTANCE_PHYSICAL) + boni[0])
-				item.settag('res_fire', fromitem(item, RESISTANCE_FIRE) + boni[1])
-				item.settag('res_cold', fromitem(item, RESISTANCE_COLD) + boni[2])
-				item.settag('res_energy', fromitem(item, RESISTANCE_ENERGY) + boni[3])
-				item.settag('res_poison', fromitem(item, RESISTANCE_POISON) + boni[4])
-			elif itemcheck(item, ITEM_WEAPON):
-				# Increase the damage bonus by 20%
-				bonus = fromitem(item, DAMAGEBONUS)
-				bonus += 20
-				item.settag('aos_boni_damage', bonus)
+			# increase damage of exceptional weapons
+			if exceptional:
+				if itemcheck(item, ITEM_WEAPON):
+					# Increase the damage bonus by 20%
+					bonus = fromitem(item, DAMAGEBONUS)
+					bonus += 20
+					item.settag('aos_boni_damage', bonus)
 
 	#
 	# Play a simple soundeffect
@@ -137,10 +178,32 @@ class SeFletchItemAction(FletchItemAction):
 class BowcraftMenu(MakeMenu):
 	def __init__(self, id, parent, title):
 		MakeMenu.__init__(self, id, parent, title)
-		self.allowmark = 1
-		self.allowrepair = 1
-		self.gumptype = 0x4f1ba412 # This should be unique
+		self.allowmark = True
+		self.allowrepair = True
+		self.submaterials1 = WOOD
+		self.submaterial1missing = 1042081
+		self.submaterial1noskill = 500298
+		self.gumptype = 0x4f1bff56 # This should be unique
 		self.requiretool = True
+
+	#
+	# Get the material used by the character from the tags
+	#
+	def getsubmaterial1used(self, player, arguments):
+		if not player.hastag('bowcraft_wood'):
+			return False
+		else:
+			material = int(player.gettag('bowcraft_wood'))
+			if material < len(self.submaterials1):
+				return material
+			else:
+				return False
+
+	#
+	# Save the material preferred by the user in a tag
+	#
+	def setsubmaterial1used(self, player, arguments, material):
+		player.settag('bowcraft_wood', material)
 
 	#
 	# Repair an item
@@ -209,7 +272,7 @@ class BowcraftMenu(MakeMenu):
 # append it to the parents submenus.
 #
 def loadMenu(id, parent = None):
-	definition = wolfpack.getdefinition(WPDT_MENU, id)
+	definition = getdefinition(WPDT_MENU, id)
 	if not definition:
 		if parent:
 			console.log(LOG_ERROR, "Unknown submenu %s in menu %s.\n" % (id, parent.id))
@@ -233,20 +296,20 @@ def loadMenu(id, parent = None):
 		# Craft an item
 		elif child.name in ['fletch', 'sefletch']:
 			if not child.hasattribute('definition'):
-				console.log(LOG_ERROR, "Fletch action without definition in menu %s.\n" % menu.id)
+				console.log(LOG_ERROR, "Bowcraft action without definition in menu %s.\n" % menu.id)
 			else:
 				itemdef = child.getattribute('definition')
 				try:
 					# See if we can find an item id if it's not given
 					if not child.hasattribute('itemid'):
-						item = wolfpack.getdefinition(WPDT_ITEM, itemdef)
+						item = getdefinition(WPDT_ITEM, itemdef)
 						itemid = 0
 						if item:
 							itemchild = item.findchild('id')
 							if itemchild:
 								itemid = itemchild.value
 						else:
-							console.log(LOG_ERROR, "Fletch action with invalid definition %s in menu %s.\n" % (itemdef, menu.id))
+							console.log(LOG_ERROR, "Bowcraft action with invalid definition %s in menu %s.\n" % (itemdef, menu.id))
 					else:
 						itemid = hex2dec(child.getattribute('itemid', '0'))
 					if child.hasattribute('name'):
@@ -258,11 +321,15 @@ def loadMenu(id, parent = None):
 					else:
 						action = FletchItemAction(menu, name, int(itemid), itemdef)
 				except:
-					console.log(LOG_ERROR, "Fletch action with invalid item id in menu %s.\n" % menu.id)
+					console.log(LOG_ERROR, "Bowcraft action with invalid item id in menu %s.\n" % menu.id)
 
 				# Process subitems
 				for j in range(0, child.childcount):
 					subchild = child.getchild(j)
+
+					# How much of the primary resource should be consumed
+					if subchild.name == 'logs':
+						action.submaterial1 = hex2dec(subchild.getattribute('amount', '0'))
 
 					# Normal Material
 					if subchild.name == 'material':
@@ -282,11 +349,11 @@ def loadMenu(id, parent = None):
 					# Consume all available materials scaled by the
 					# amount of each submaterial
 					elif subchild.name == 'stackable':
-						action.stackable = 1
+						action.stackable = True
 
 					# This item will never be exceptional
 					elif subchild.name == 'nomark':
-						action.markable = 0
+						action.markable = False
 
 					# Skill requirement
 					elif subchild.name in skillnamesids:
@@ -312,7 +379,7 @@ def loadMenu(id, parent = None):
 	menu.sort()
 
 #
-# Load the blacksmithing menu.
+# Load the bowcraft menu.
 #
 def onLoad():
 	loadMenu('BOWCRAFT')
