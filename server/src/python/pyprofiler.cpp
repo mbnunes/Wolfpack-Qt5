4cb4b444f1cab81f2771c66d7aa3a7ec6305843b
correa <correa@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2005-12-25 21:11:24 +0000

    missing some files

---
+++ server/src/python/pyprofiler.cpp
@@ -0,0 +1,66 @@
+/*
+*     Wolfpack Emu (WP)
+* UO Server Emulation Program
+*
+* Copyright 2001-2005 by holders identified in AUTHORS.txt
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Palace - Suite 330, Boston, MA 02111-1307, USA.
+*
+* In addition to that license, if you are running this program or modified
+* versions of it on a public system you HAVE TO make the complete source of
+* the version used by you available or provide people with a location to
+* download it.
+*
+* Wolfpack Homepage: http://developer.berlios.de/projects/wolfpack/
+*/
+
+
+#include "pyprofiler.h"
+#include <QByteArray>
+
+int PyProfiler::tracefunc( PyObject *obj, PyFrameObject *frame, int what, PyObject *arg )
+{
+	PyProfiler* that = PyProfiler::instance();
+	ProfileData* pdata = 0; 
+	switch ( what )
+	{
+	case PyTrace_CALL:
+		//that->data.find()
+		// Don't count recursions
+		pdata = &that->data[PyString_AS_STRING(frame->f_code->co_filename)][PyString_AS_STRING(frame->f_code->co_name)];	
+		pdata.firstline = frame->f_code->co_firstlineno;
+		if ( frame != frame->f_back )
+			pdata.calls++;
+		//that->data[PyString_AS_STRING(frame->f_code->co_filename)][PyString_AS_STRING(frame->f_code->co_name)] = pdata;
+		
+	case PyTrace_RETURN:
+	case PyTrace_C_CALL:
+	case PyTrace_C_RETURN:
+	default:
+		break;
+	}
+	return 0;
+}
+
+void PyProfiler::start()
+{
+	data.clear();
+	PyEval_SetProfile( (Py_tracefunc)PyProfiler::tracefunc, 0 );
+}
+
+void PyProfiler::stop()
+{
+	PyEval_SetProfile( 0, 0 );
+	emit stopped();
+}
---
+++ server/src/python/pyprofiler.h
@@ -0,0 +1,70 @@
+/*
+*     Wolfpack Emu (WP)
+* UO Server Emulation Program
+*
+* Copyright 2001-2005 by holders identified in AUTHORS.txt
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Palace - Suite 330, Boston, MA 02111-1307, USA.
+*
+* In addition to that license, if you are running this program or modified
+* versions of it on a public system you HAVE TO make the complete source of
+* the version used by you available or provide people with a location to
+* download it.
+*
+* Wolfpack Homepage: http://developer.berlios.de/projects/wolfpack/
+*/
+
+
+#if !defined ( __PYPROFILER_H__ )
+#define __PYPROFILER_H__
+
+#include "../singleton.h"
+#include <Python.h>
+#include <frameobject.h>
+
+#include <QHash>
+
+class ProfileData
+{
+public:
+	uint timming;
+	uint calls;
+	uint firstline;
+};
+
+typedef QHash< QByteArray, QMap< QByteArray, ProfileData > > ProfileSession;
+
+class PyProfiler : public QObject, public cComponent, public Singleton<PyProfiler>
+{
+	Q_OBJECT
+
+	ProfileSession data;
+public:
+
+	ProfileSession sessionData() const { return data; }
+
+public slots:
+	void start();
+	void stop();
+	
+signals:
+	void stopped();
+
+private:
+	static int tracefunc( PyObject *obj, PyFrameObject *frame, int what, PyObject *arg );
+};
+
+
+
+#endif // __PYPROFILER_H__
