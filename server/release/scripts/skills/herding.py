3418db7a30559ae637c93fd0c274f2ff7479b1f3
naddel <naddel@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2006-01-01 18:31:41 +0000

    shepherds crook

---
+++ server/release/scripts/skills/herding.py
@@ -0,0 +1,45 @@
+# Herding
+import wolfpack
+import wolfpack.utilities
+from wolfpack.consts import GRAY
+from wolfpack.consts import *
+
+def onUse( char, item ):
+	# Needs to be on ourself
+	if item.getoutmostchar() != char:
+		char.socket.clilocmessage( 500312, '', GRAY ) # You cannot reach that.
+		return False
+	char.socket.clilocmessage( 502464 ) # Target the animal you wish to herd.
+	char.socket.attachtarget( "skills.herding.response", [] )
+	return True
+
+def response( char, args, target ):
+	if target.char:
+		if not char.canreach(target.char, 10):
+			return False
+		# animal
+		if target.char.bodytype == 3:
+			char.socket.clilocmessage( 502475 ) # Click where you wish the animal to go.
+			char.socket.attachtarget( "skills.herding.gothere", [ target.char.serial ] )
+		else:
+			char.socket.clilocmessage( 502468 ) # That is not a herdable animal.
+			return False
+	else:
+		char.socket.clilocmessage( 502472 ) # You don't seem to be able to persuade that to move.
+		return False
+	return True
+
+def gothere(char, args, target):
+	pet = wolfpack.findchar(args[0])
+	if not pet:
+		return False
+	if not char.canreach(target.pos, 10):
+		return False
+	if not char.checkskill(HERDING, 0, 1000):
+		char.socket.clilocmessage( 502472 ) # You don't seem to be able to persuade that to move.
+		return False
+
+	char.socket.clilocmessage( 502479 ) # The animal walks where it was instructed to.
+	pet.goto(target.pos)
+	pet.sound(SND_ATTACK)   
+	return
