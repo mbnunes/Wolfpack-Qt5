701d0ee1b6ffca19e51d8e0285676ae9a843c1d8
naddel <naddel@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2006-01-07 21:13:59 +0000

    statuettes in minor artifacts (MagnusBR)

--- server/release/scripts/statuette.py
+++ server/release/scripts/statuette.py
@@ -3,37 +3,37 @@ import wolfpack.gumps
 
 # ids = { id : [name, soundid] }
 ids = {
-	0x20DA : [1041249 , 660], # Crocodile
-	0x20D3 : [1041250, 357], # Daemon
-	0x20D6 : [1041251, 362], # Dragon
-	0x20D7 : [1041252, 268], # EarthElemental
-	0x20D8 : [1041253, 367], # Ettin
-	0x20D9 : [1041254, 372], # Gargoyle
-	0x20F5 : [1041255, 158], # Gorilla
-	0x20F8 : [1041256, 1001], # Lich
-	0x20DE : [1041257, 417], # Lizardman	
-	0x20DF : [1041258, 427], # Ogre
-	0x20E0 : [1041259, 1114], # Orc
-	0x20E3 : [1041260, 437], # Ratman
-	0x20E7 : [1041261, 1165], # Skeleton
-	0x20E9 : [1041262, 461], # Troll
+	0x20da : [1041249 , 660], # Crocodile
+	0x20d3 : [1041250, 357], # Daemon
+	0x20d6 : [1041251, 362], # Dragon
+	0x20d7 : [1041252, 268], # EarthElemental
+	0x20d8 : [1041253, 367], # Ettin
+	0x20d9 : [1041254, 372], # Gargoyle
+	0x20f5 : [1041255, 158], # Gorilla
+	0x20f8 : [1041256, 1001], # Lich
+	0x20de : [1041257, 417], # Lizardman	
+	0x20df : [1041258, 427], # Ogre
+	0x20e0 : [1041259, 1114], # Orc
+	0x20e3 : [1041260, 437], # Ratman
+	0x20e7 : [1041261, 1165], # Skeleton
+	0x20e9 : [1041262, 461], # Troll
 	0x2103 : [1041263, 120], # Cow
-	0x20EC : [1041264, 471], # Zombie
-	0x20F6 : [1041265, 1011], # Llama
+	0x20ec : [1041264, 471], # Zombie
+	0x20f6 : [1041265, 1011], # Llama
 	0x2133 : [1049742, 634], # Ophidian
-	0x20FA : [1049743, 442], # Reaper
-	0x20F9 : [1049744, 422], # Mongbat
-	0x20F4 : [1049768, 377], # Gazer
-	0x20F3 : [1049769, 838], # FireElemental
+	0x20fa : [1049743, 442], # Reaper
+	0x20f9 : [1049744, 422], # Mongbat
+	0x20f4 : [1049768, 377], # Gazer
+	0x20f3 : [1049769, 838], # FireElemental
 	0x2122 : [1049770, 229], # Wolf
-	0x3FFE : [1063488, 205], # Phillip's Steed
-	0x25BA : [1070819, 138]  # Seahorse
+	0x3ffe : [1063488, 205], # Phillip's Steed
+	0x25ba : [1070819, 138]  # Seahorse
 }
 
 def onUse(char, item):
 	if not item.getoutmostchar() == char:
 		char.socket.clilocmessage(502691) # You must be the owner to use this.
-		return False
+		return True
 	sendgump(char, item)
 	return True
 
@@ -74,6 +74,8 @@ def gump_callback(player, arguments, response):
 	return True
 
 def onShowTooltip(player, object, tooltip):
+	if not object.id in ids.keys():
+		return False
 	tooltip.reset()
 	name = ids[object.id][0]
 	tooltip.add(name, '')
