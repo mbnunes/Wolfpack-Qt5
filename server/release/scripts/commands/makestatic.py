9794f12c55afa3f9f8a96b10344d67b52e4f97a7
magnusbr <magnusbr@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2006-03-06 19:02:02 +0000

    - Added "makestatic" command. It could be used to change an item to "Static" status (As items added by static command)

---
+++ server/release/scripts/commands/makestatic.py
@@ -0,0 +1,37 @@
+#===============================================================#
+#   )      (\_     | WOLFPACK 13.0.0 Scripts
+#  ((    _/{  "-;  | Created by: MagnusBr
+#   )).-' {{ ;'`   | Revised by:
+#  ( (  ;._ \\ ctr | Last Modification: Created
+#===============================================================#
+# .dye Command
+#===============================================================#
+
+"""
+	\command makestatic
+	\description Make an Item static.
+	\usage - <code>freeze</code>
+"""
+
+import wolfpack
+from wolfpack.utilities import booleantoggle
+from wolfpack.consts import GRAY
+
+def makestatic( socket, command, arguments ):
+	try:
+		socket.attachtarget( "commands.makestatic.response", [] )
+	except:
+		socket.sysmessage( 'Usage: makestatic' )
+
+def response( char, args, target ):
+	if target.item:
+		target.item.movable = 3
+		target.item.decay = False
+		char.socket.sysmessage( 'Object is now Static.', GRAY )
+	else:
+		char.socket.sysmessage( 'That was not a valid object.', GRAY )
+	return
+
+def onLoad():
+	wolfpack.registercommand( "makestatic", makestatic )
+	return
