4f4b92ca841b7ad512096e526fca99dd98ce64aa
magnusbr <magnusbr@db57ef4e-4fe8-0310-84ee-c23f6f2f8b61>
2006-08-05 05:42:52 +0000

    Worked on Tracking Skill. Its near complete now. I just have to add a "close quest arrow packet" support on core.

--- server/release/scripts/skills/tracking.py
+++ server/release/scripts/skills/tracking.py
@@ -12,7 +12,7 @@ import wolfpack
 import wolfpack.time
 import skills
 import random
-from wolfpack.consts import TRACKING
+from wolfpack.consts import TRACKING, HIDING, DETECTINGHIDDEN, STEALTH
 from wolfpack.gumps import cGump
 from wolfpack.consts import GRAY
 from wolfpack import tr
@@ -34,6 +34,12 @@ elves_tracking_chance = int( wolfpack.settings.getnumber("Racial Features", "Elv
 #1002166 The tracking skill will allow you to track animals, monsters, and people. Use the skill by clicking the jewel on your skill list. You will be presented with a window, which will allow you to choose what you wish to track. Double-click the category you wish to track (animal, human, or monster), and you will be presented with a second window listing the specific PCs or NPC's that you can track. If you are successful, and an appropriate creature can be tracked, you will be told in what direction that creature can be found. The higher your skill level
 #1005645 I did not find a nearby track piece
 
+# Some comments from stratics to implement some time:
+# Necromancers can be a bit harder or easier depending on which form they aren in.
+# Vampires always have a minimum of 50, if their Hiding + Stealth is less than 50. 
+# The big blue four-armed demon gets a penalty of 20. It's so big it's easier to track. 
+# Wraiths and Banshees get a bonus of 20 to their difficulty, if they're below 200. 
+
 TRACKING_DELAY = 5000
 
 def tracking( char, skill ):
@@ -48,6 +54,12 @@ def tracking( char, skill ):
 			return True
 		else:
 			socket.deltag( 'skill_delay' )
+
+	if char.socket.hastag('usingtracking'):
+		socket.sysmessage('You stop tracking the target')
+		char.socket.questarrow(False, 0, 0)
+		char.socket.deltag('usingtracking')
+		return True
    
 	socket.clilocmessage( 1011350 ) # What do you wish to track?
 	socket.closegump( 0x87651592 ) # What to track
@@ -115,12 +127,19 @@ def trackWhatResponse( char, args, target ):
 		# NEVER track invisible staff members
 		if not charcible.invisible and not charcible.dead:
 			if target.button == 4 : #have to exit non-players and exit self
-				if charcible.player :
-					if charcible.elf:
-						if random.randint(1,100) > elves_tracking_chance:
-							liste.append(charcible)
+				if charcible.player and not charcible == char:
+					# Tracking Chances
+					if ( charcible.skill[HIDING] + charcible.skill[STEALTH] ) > 0:
+						chance = int( ( char.skill[TRACKING] + char.skill[DETECTINGHIDDEN] + random.randint(10,200) ) / ( charcible.skill[HIDING] + charcible.skill[STEALTH] ) * 50 )
 					else:
+						chance = 100
+	
+					if charcible.elf:
+						chance -= ( chance * elves_tracking_chance ) / 100
+					
+					if random.randint(1,100) < chance:
 						liste.append(charcible)
+
 			if target.button == 3 : #have to exit non-PNJ humans
 				if charcible.bodytype==4 and charcible.npc:
 					liste.append(charcible)
@@ -141,10 +160,114 @@ def trackWhatResponse( char, args, target ):
 		else:
 			char.socket.clilocmessage( 502993, '', GRAY ) # You see no evidence of creatures in the area.
 	else: #list is not empty
-		for charcible in liste:
-			char.socket.sysmessage("%s %s"%(charcible.name,dircard(char.directionto(charcible))))
+
+		createsecondmenu(char, liste, 1)
+
+		#for charcible in liste:
+		#	char.socket.sysmessage("%s %s"%(charcible.name,dircard(char.directionto(charcible))))
+	
 	#check skill
 	success = char.checkskill( TRACKING, 0, 1000 )
+	return True
+
+def createsecondmenu(char, liste, startpoint):
+
+	socket = char.socket
+
+	socket.closegump( 0x87651592 ) # What to track
+	gump = cGump( x=20, y=30, callback=trackWhatResponse2, type=0x87651592 )
+
+	gump.startPage( 0 )
+	gump.addBackground( 5054, 440, 135 )
+	gump.addResizeGump( 10, 10, 2620, 420, 75 )
+	gump.addResizeGump( 10, 85, 3000, 420, 25 )
+
+	# Last and next buttons
+	if ( len(liste) - startpoint + 1 ) > 4:
+		gump.addButton(415, 115, 5224, 5224, 200)
+
+	if startpoint > 4:
+		gump.addButton(5, 115, 5223, 5223, 201)
+	
+	# First Char
+	if not len(liste) < startpoint:
+		npc1 = liste[startpoint - 1]
+		bodyinfo = wolfpack.bodyinfo(npc1.id)
+		showid1 = bodyinfo['figurine']	
+
+		gump.addTilePic( 20, 20, showid1 )
+		gump.addButton( 25, 110, 4005, 4007, startpoint )
+		gump.addText(24, 90, npc1.name, 0)
+
+	# Second Char
+	if not len(liste) < ( startpoint + 1 ):
+		npc2 = liste[startpoint]
+		bodyinfo = wolfpack.bodyinfo(npc2.id)
+		showid2 = bodyinfo['figurine']
+
+		gump.addTilePic( 120, 20, showid2 )
+		gump.addButton( 125, 110, 4005, 4007, startpoint + 1 )
+		gump.addText(120, 90, npc2.name, 0)
+
+	# Third Char
+	if not len(liste) < ( startpoint + 2 ):
+		npc3 = liste[startpoint + 1]
+		bodyinfo = wolfpack.bodyinfo(npc3.id)
+		showid3 = bodyinfo['figurine']
+
+		gump.addTilePic( 220, 20, showid3 )
+		gump.addButton( 225, 110, 4005, 4007, startpoint + 2 )
+		gump.addText(220, 90, npc3.name, 0)
+
+	# Last Char
+	if not len(liste) < ( startpoint + 3 ):
+		npc4 = liste[startpoint + 2]
+		bodyinfo = wolfpack.bodyinfo(npc4.id)
+		showid4 = bodyinfo['figurine']
+
+		gump.addTilePic( 320, 20, showid4 )
+		gump.addButton( 325, 110, 4005, 4007, startpoint + 3 )
+		gump.addText(320, 90, npc4.name, 0)
+
+	gump.setArgs( [liste, startpoint] )
+
+	gump.send( char )
+
+	if skills.skilltable[ TRACKING ][ skills.UNHIDE ] and char.hidden:
+		char.reveal()
+	return True
+
+def trackWhatResponse2( char, args, target ):
+
+	# Retrieve List and Startpoint
+	list = args[0]
+	startpoint = args[1]
+	
+	# Next and last buttons
+	if target.button == 200:
+		createsecondmenu(char, list, startpoint + 4)
+		return True
+
+	if target.button == 201:
+		createsecondmenu(char, list, startpoint - 4)
+		return True
+
+	# Button is 0?
+	if target.button == 0:
+		return True
+
+	# Lets show the NPC position
+	npc = list[target.button - 1]
+
+	# Send the Arrow
+	char.socket.questarrow(True, npc.pos.x, npc.pos.y)
+
+	# Assign that we are using Tracking now. On RunUO I think a reverse click make arrow disappear... but its not supported on core yet (Ill work on it later). Here, to turn off the arrow we have to use Tracking again
+	char.socket.settag('usingtracking', 1)
+
+	# Message
+	char.socket.sysmessage('Tracking the target. Use tracking Skill again to turn off the arrow...')
+
 	return True
 
 def onLoad():
